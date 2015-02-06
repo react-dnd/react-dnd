95a00eaabe07a5f4818a90cdc5ceca7828ff187c
Dan Abramov <dan.abramov@gmail.com>
2015-02-06 21:02:01 +0300

    Dragging works!

--- modules/backends/MouseMoveBackend.js
+++ modules/backends/MouseMoveBackend.js
@@ -1,22 +1,45 @@
 'use strict';
 
-var DragDropActionCreators = require('../actions/DragDropActionCreators');
+var DragDropActionCreators = require('../actions/DragDropActionCreators'),
+    DragDropStore = require('../stores/DragDropStore');
+
+var _currentComponent;
+
+function handleTopMouseMove(e) {
+  var type = DragDropStore.getDraggedItemType();
+  _currentComponent.handleDrag(type, e);
+}
+
+function handleTopMouseUp() {
+  var type = DragDropStore.getDraggedItemType();
+  _currentComponent.handleDragEnd(type, null);
+}
 
 var MouseMoveBackend = {
   setup() {
-    if (typeof window !== 'undefined') {
+    if (typeof window === 'undefined') {
+      return;
     }
+
+    window.addEventListener('mousemove', handleTopMouseMove);
+    window.addEventListener('mouseup', handleTopMouseUp);
   },
 
   teardown() {
-    if (typeof window !== 'undefined') {
+    if (typeof window === 'undefined') {
+      return;
     }
+
+    window.addEventListener('mousemove', handleTopMouseMove);
+    window.addEventListener('mouseup', handleTopMouseUp);
   },
 
-  beginDrag(e, containerNode, dragPreview, dragAnchors, dragStartOffset, effectsAllowed, imitateDragEnd) {
+  beginDrag(component, e, containerNode, dragPreview, dragAnchors, dragStartOffset, effectsAllowed) {
+    _currentComponent = component;
   },
 
   endDrag() {
+    _currentComponent = null;
   },
 
   dragOver(e, dropEffect) {
@@ -25,9 +48,7 @@ var MouseMoveBackend = {
   getDragSourceProps(component, type) {
     // TODO: optimize bind when we figure this out
     return {
-      onMouseDown: component.handleDragStart.bind(component, type),
-      onMouseMove: component.handleDrag.bind(component, type),
-      onMouseUp: component.handleDragEnd.bind(component, type)
+      onMouseDown: component.handleDragStart.bind(component, type)
     };
   },
 
