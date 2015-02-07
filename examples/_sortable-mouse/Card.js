5e7dd68235d1a54658d2e307ed2449090deaf663
Nathan Hutchison <nathan@bugherd.com>
2015-02-07 15:25:25 +1100

    sortable example using mouse backend

---
+++ examples/_sortable-mouse/Card.js
@@ -0,0 +1,57 @@
+'use strict';
+
+var React = require('react'),
+    ItemTypes = require('./ItemTypes'),
+    { PropTypes } = React,
+    { MouseDragDropMixin, DropEffects } = require('react-dnd');
+
+var Card = React.createClass({
+  mixins: [MouseDragDropMixin],
+
+  propTypes: {
+    id: PropTypes.any.isRequired,
+    text: PropTypes.string.isRequired,
+    moveCard: PropTypes.func.isRequired
+  },
+
+  configureDragDrop(registerType) {
+    registerType(ItemTypes.CARD, {
+      dragSource: {
+        beginDrag() {
+          return {
+            item: {
+              id: this.props.id,
+              children: <Card {...this.props}/>
+            }
+          };
+        }
+      },
+
+      dropTarget: {
+        over(item) {
+          this.props.moveCard(item.id, this.props.id);
+        }
+      }
+    });
+  },
+
+  render() {
+    var { isDragging } = this.getDragState(ItemTypes.CARD);
+
+    return (
+      <div {...this.dragSourceFor(ItemTypes.CARD)}
+           {...this.dropTargetFor(ItemTypes.CARD)}
+           style={{
+             border: '1px dashed gray',
+             backgroundColor: 'white',
+             padding: '0.5rem',
+             margin: '0.5rem',
+             opacity: isDragging ? 0 : 1
+           }}>
+        {this.props.text}
+      </div>
+    );
+  }
+});
+
+module.exports = Card;
