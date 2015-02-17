e9b06b6bef6f923f851523839bd14f50125031d9
scante <scante@ipowow.com>
2015-02-17 15:26:04 +1100

    Allow drop into it self

--- modules/mixins/DragDropMixin.js
+++ modules/mixins/DragDropMixin.js
@@ -134,10 +134,11 @@ var DragDropMixin = {
     if (!dropTarget) {
       return null;
     }
-
-    if (draggedItemType === ownDraggedItemType) {
-      return null;
-    }
+    // https://github.com/gaearon/react-dnd/issues/35
+    // ALLOW to drop into itself 
+    // if (draggedItemType === ownDraggedItemType) {
+    //   return null;
+    // }
 
     var { canDrop } = dropTarget;
     return callDragDropLifecycle(canDrop, this, draggedItem) ? draggedItemType : null;
