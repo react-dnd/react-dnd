9a5e74f61fa9da999c2c9e76b4537df0ed845e33
Dan Abramov <dan.abramov@gmail.com>
2015-03-10 21:57:11 +0300

    Only interpret dragstart as dragging a link if no drag source caught an event Fixes #113

---
+++ modules/utils/isNativeDraggedItemType.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var NativeDragItemTypes = require('../constants/NativeDragItemTypes');
+
+function isNativeDraggedItemType(itemType) {
+  switch (itemType) {
+  case NativeDragItemTypes.FILE:
+  case NativeDragItemTypes.URL:
+    return true;
+  default:
+    return false;
+  }
+}
+
+module.exports = isNativeDraggedItemType;
