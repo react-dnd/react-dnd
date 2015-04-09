fe1b73e341390363e54a4afadaa21452af78ae1f
Dan Abramov <dan.abramov@gmail.com>
2015-04-09 13:11:12 +0300

    Extract module

--- modules/backends/HTML5.js
+++ modules/backends/HTML5.js
@@ -1,10 +1,15 @@
 import { DragSource } from 'dnd-core';
 import NativeTypes from '../NativeTypes';
 import EnterLeaveCounter from '../utils/EnterLeaveCounter';
+import createMonotonicInterpolant from '../utils/createMonotonicInterpolant';
 import shallowEqual from '../utils/shallowEqual';
 import defaults from 'lodash/object/defaults';
+import memoize from 'lodash/function/memoize';
 import invariant from 'react/lib/invariant';
 import warning from 'react/lib/warning';
+
+const isSafari = memoize(() => Boolean(window.safari));
+const isFirefox = memoize(() => /firefox/i.test(navigator.userAgent));
 
 function isUrlDataTransfer(dataTransfer) {
   var types = Array.prototype.slice.call(dataTransfer.types);
@@ -40,110 +45,6 @@ function getMouseEventOffsets(e, sourceNode, dragPreview) {
   return { offsetFromClient, offsetFromSource, offsetFromDragPreview };
 }
 
-function isDesktopSafari() {
-  return !!window.safari;
-}
-
-function isFirefox() {
-  return /firefox/i.test(navigator.userAgent);
-}
-
-/**
- * I took this straight from Wikipedia, it must be good!
- */
-function createMonotonicInterpolant(xs, ys) {
-  const length = xs.length;
-
-  // Rearrange xs and ys so that xs is sorted
-  const indexes = [];
-  for (let i = 0; i < length; i++) {
-    indexes.push(i);
-  }
-  indexes.sort((a, b) => xs[a] < xs[b] ? -1 : 1);
-
-  const oldXs = xs, oldYs = ys;
-  // Impl: Creating new arrays also prevents problems if the input arrays are mutated later
-  xs = [];
-  ys = [];
-  // Impl: Unary plus properly converts values to numbers
-  for (let i = 0; i < length; i++) {
-    xs.push(+oldXs[indexes[i]]);
-    ys.push(+oldYs[indexes[i]]);
-  }
-
-  // Get consecutive differences and slopes
-  const dys = [];
-  const dxs = [];
-  const ms = [];
-  let dx, dy;
-  for (let i = 0; i < length - 1; i++) {
-    dx = xs[i + 1] - xs[i];
-    dy = ys[i + 1] - ys[i];
-    dxs.push(dx);
-    dys.push(dy);
-    ms.push(dy / dx);
-  }
-
-  // Get degree-1 coefficients
-  const c1s = [ms[0]];
-  for (let i = 0; i < dxs.length - 1; i++) {
-    const m = ms[i];
-    const mNext = ms[i + 1];
-    if (m * mNext <= 0) {
-      c1s.push(0);
-    } else {
-      dx = dxs[i];
-      const dxNext = dxs[i + 1];
-      const common = dx + dxNext;
-      c1s.push(3 * common / ((common + dxNext) / m + (common + dx) / mNext));
-    }
-  }
-  c1s.push(ms[ms.length - 1]);
-
-  // Get degree-2 and degree-3 coefficients
-  const c2s = [];
-  const c3s = [];
-  let m;
-  for (let i = 0; i < c1s.length - 1; i++) {
-    m = ms[i];
-    const c1 = c1s[i];
-    const invDx = 1 / dxs[i];
-    const common = c1 + c1s[i + 1] - m - m;
-    c2s.push((m - c1 - common) * invDx);
-    c3s.push(common * invDx * invDx);
-  }
-
-  // Return interpolant function
-  return function (x) {
-    // The rightmost point in the dataset should give an exact result
-    let i = xs.length - 1;
-    if (x === xs[i]) {
-      return ys[i];
-    }
-
-    // Search for the interval x is in, returning the corresponding y if x is one of the original xs
-    let low = 0;
-    let high = c3s.length - 1;
-    let mid;
-    while (low <= high) {
-      mid = Math.floor(0.5 * (low + high));
-      const xHere = xs[mid];
-      if (xHere < x) {
-        low = mid + 1;
-      } else if (xHere > x) {
-        high = mid - 1;
-      } else {
-        return ys[mid];
-      }
-    }
-    i = Math.max(0, high);
-
-    // Interpolate
-    const diff = x - xs[i], diffSq = diff * diff;
-    return ys[i] + c1s[i] * diff + c2s[i] * diffSq + c3s[i] * diff * diffSq;
-  };
-}
-
 function getDragPreviewOffset(sourceNode, dragPreview, offsetFromDragPreview, anchorPoint) {
   const { offsetWidth: sourceWidth, offsetHeight: sourceHeight } = sourceNode;
   const { anchorX, anchorY } = anchorPoint;
@@ -153,7 +54,7 @@ function getDragPreviewOffset(sourceNode, dragPreview, offsetFromDragPreview, an
   let dragPreviewHeight = isImage ? dragPreview.height : sourceHeight;
 
   // Work around @2x coordinate discrepancies in browsers
-  if (isDesktopSafari() && isImage) {
+  if (isSafari() && isImage) {
     dragPreviewHeight /= window.devicePixelRatio;
     dragPreviewWidth /= window.devicePixelRatio;
   } else if (isFirefox() && !isImage) {
@@ -183,7 +84,7 @@ function getDragPreviewOffset(sourceNode, dragPreview, offsetFromDragPreview, an
   let y = interpolateY(anchorY);
 
   // Work around Safari 8 positioning bug
-  if (isDesktopSafari() && isImage) {
+  if (isSafari() && isImage) {
     // We'll have to wait for @3x to see if this is entirely correct
     y += (window.devicePixelRatio - 1) * dragPreviewHeight;
   }
