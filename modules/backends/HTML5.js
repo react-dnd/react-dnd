b6eace82ee236ece876d9bda6d62ece4119069a3
Dan Abramov <dan.abramov@gmail.com>
2015-04-09 13:31:45 +0300

    Move connect to the top

--- modules/backends/HTML5.js
+++ modules/backends/HTML5.js
@@ -129,6 +129,54 @@ export default class HTML5Backend {
     this.clearCurrentDragSourceNode();
   }
 
+  connect() {
+    return {
+      dragSource: this.connectSourceNode,
+      dragSourcePreview: this.connectSourcePreviewNode,
+      dropTarget: this.connectTargetNode
+    };
+  }
+
+  connectSourcePreviewNode(sourceId, node, options) {
+    this.sourcePreviewNodeOptions[sourceId] = options;
+    this.sourcePreviewNodes[sourceId] = node;
+
+    return () => {
+      delete this.sourcePreviewNodes[sourceId];
+      delete this.sourcePreviewNodeOptions[sourceId];
+    };
+  }
+
+  connectSourceNode(sourceId, node, options) {
+    const handleDragStart = (e) => this.handleDragStart(e, sourceId);
+
+    this.sourceNodeOptions[sourceId] = options;
+    node.setAttribute('draggable', true);
+    node.addEventListener('dragstart', handleDragStart);
+
+    return () => {
+      delete this.sourceNodeOptions[sourceId];
+      node.removeEventListener('dragstart', handleDragStart);
+      node.setAttribute('draggable', false);
+    };
+  }
+
+  connectTargetNode(targetId, node) {
+    const handleDragEnter = (e) => this.handleDragEnter(e, targetId);
+    const handleDragOver = (e) => this.handleDragOver(e, targetId);
+    const handleDrop = (e) => this.handleDrop(e, targetId);
+
+    node.addEventListener('dragenter', handleDragEnter);
+    node.addEventListener('dragover', handleDragOver);
+    node.addEventListener('drop', handleDrop);
+
+    return () => {
+      node.removeEventListener('dragenter', handleDragEnter);
+      node.removeEventListener('dragover', handleDragOver);
+      node.removeEventListener('drop', handleDrop);
+    };
+  }
+
   getSpecifiedDropEffect() {
     const sourceId = this.monitor.getSourceId();
     const sourceNodeOptions = this.sourceNodeOptions[sourceId];
@@ -432,53 +480,5 @@ export default class HTML5Backend {
     } else {
       this.endDragIfSourceWasRemovedFromDOM();
     }
-  }
-
-  connect() {
-    return {
-      dragSource: this.connectSourceNode,
-      dragSourcePreview: this.connectSourcePreviewNode,
-      dropTarget: this.connectTargetNode
-    };
-  }
-
-  connectSourcePreviewNode(sourceId, node, options) {
-    this.sourcePreviewNodeOptions[sourceId] = options;
-    this.sourcePreviewNodes[sourceId] = node;
-
-    return () => {
-      delete this.sourcePreviewNodes[sourceId];
-      delete this.sourcePreviewNodeOptions[sourceId];
-    };
-  }
-
-  connectSourceNode(sourceId, node, options) {
-    const handleDragStart = (e) => this.handleDragStart(e, sourceId);
-
-    this.sourceNodeOptions[sourceId] = options;
-    node.setAttribute('draggable', true);
-    node.addEventListener('dragstart', handleDragStart);
-
-    return () => {
-      delete this.sourceNodeOptions[sourceId];
-      node.removeEventListener('dragstart', handleDragStart);
-      node.setAttribute('draggable', false);
-    };
-  }
-
-  connectTargetNode(targetId, node) {
-    const handleDragEnter = (e) => this.handleDragEnter(e, targetId);
-    const handleDragOver = (e) => this.handleDragOver(e, targetId);
-    const handleDrop = (e) => this.handleDrop(e, targetId);
-
-    node.addEventListener('dragenter', handleDragEnter);
-    node.addEventListener('dragover', handleDragOver);
-    node.addEventListener('drop', handleDrop);
-
-    return () => {
-      node.removeEventListener('dragenter', handleDragEnter);
-      node.removeEventListener('dragover', handleDragOver);
-      node.removeEventListener('drop', handleDrop);
-    };
   }
 }
