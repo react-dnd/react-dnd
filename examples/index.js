f1afb0322dd12b41bb24d221de10e51120045dea
Dan Abramov <dan.abramov@gmail.com>
2015-04-18 15:42:17 +0300

    Disable all but the simplest example

--- examples/index.js
+++ examples/index.js
@@ -4,6 +4,7 @@ import React from 'react';
 import Router, { Route, Link, Redirect, RouteHandler } from 'react-router';
 import AnimationFrame from 'animation-frame';
 import DustbinSimple from './_dustbin-simple';
+/*
 import DustbinInteresting from './_dustbin-interesting';
 import DustbinStress from './_dustbin-stress';
 import NestingSources from './_nesting-sources';
@@ -11,6 +12,7 @@ import SortableSimple from './_sortable-simple';
 import SortableStress from './_sortable-stress';
 import CustomizeHandles from './_customize-handles';
 import CustomizeEffects from './_customize-effects';
+*/
 
 /*
 DragAroundNaive = require('./_drag-around-naive/index'),
@@ -24,10 +26,6 @@ const App = React.createClass({
       <div>
         <h1>react-dnd examples (<a target='_href' href='https://github.com/gaearon/react-dnd/blob/master/examples'>source</a>)</h1>
         <ul>
-          <li>Dustbin (<Link to='dustbin-simple'>simple</Link>, <Link to='dustbin-interesting'>interesting</Link>, <Link to='dustbin-stress'>stress test</Link>)</li>
-          <li>Nesting (<Link to='nesting-sources'>drag sources</Link>)</li>
-          <li>Sortable (<Link to='sortable-simple'>simple</Link>, <Link to='sortable-stress'>stress test</Link>)</li>
-          <li>Customize (<Link to='customize-handles'>drag handles</Link>, <Link to='customize-effects'>drop effects</Link>)</li>
         </ul>
         <hr />
         <RouteHandler />
@@ -37,6 +35,13 @@ const App = React.createClass({
 });
 
 /*
+
+          <li>Dustbin (<Link to='dustbin-simple'>simple</Link>, <Link to='dustbin-interesting'>interesting</Link>, <Link to='dustbin-stress'>stress test</Link>)</li>
+          <li>Nesting (<Link to='nesting-sources'>drag sources</Link>)</li>
+          <li>Sortable (<Link to='sortable-simple'>simple</Link>, <Link to='sortable-stress'>stress test</Link>)</li>
+          <li>Customize (<Link to='customize-handles'>drag handles</Link>, <Link to='customize-effects'>drop effects</Link>)</li>
+
+
 <ul>
   <li>Drag Around (<Link to='drag-around-naive'>naive</Link>, <Link to='drag-around-custom'>custom</Link>)</li>
 </ul>
@@ -45,6 +50,11 @@ const App = React.createClass({
 const routes = (
   <Route handler={App}>
     <Route name='dustbin-simple' path='dustbin-simple' handler={DustbinSimple} />
+    <Redirect from='/' to='dustbin-simple' />
+  </Route>
+);
+
+/*
     <Route name='dustbin-interesting' path='dustbin-interesting' handler={DustbinInteresting} />
     <Route name='dustbin-stress' path='dustbin-stress' handler={DustbinStress} />
     <Route name='nesting-sources' path='nesting-sources' handler={NestingSources} />
@@ -52,9 +62,7 @@ const routes = (
     <Route name='sortable-stress' path='sortable-stress' handler={SortableStress} />
     <Route name='customize-handles' path='customize-handles' handler={CustomizeHandles} />
     <Route name='customize-effects' path='customize-effects' handler={CustomizeEffects} />
-    <Redirect from='/' to='dustbin-simple' />
-  </Route>
-);
+*/
 
 AnimationFrame.shim();
 
