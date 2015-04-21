ba8359ab3d7c76592357e078561d0e9d96afbcb0
Dan Abramov <dan.abramov@gmail.com>
2015-04-21 04:04:28 +0300

    whatever

--- examples/_drag-around-custom/index.js
+++ examples/_drag-around-custom/index.js
@@ -42,23 +42,6 @@ export default class DragAroundCustom extends Component {
             Snap to grid while dragging
           </input>
         </p>
-        <hr />
-        <p>
-          Browser APIs provide no way to change drag preview or its behavior once drag has started.
-          Libraries such as jQuery UI implement drag and drop from scratch to work around this, but react-dnd
-          only supports browser drag and drop “backend” for now, so we have to accept its limitations.
-        </p>
-        <p>
-          We can, however, customize behavior a great deal if we feed the browser an empty image as drag preview.
-          This library provides <code>DragLayerMixin</code> that you can use to implement a fixed layer on top of your app where you'd draw a custom drag preview component.
-        </p>
-        <p>
-          Note that we can draw a completely different component on our drag layer if we wish so. It's not just a screenshot.
-        </p>
-        <p>
-          With this approach, we miss out on default “return” animation when dropping outside the container.
-          However, we get great flexibility in customizing drag feedback and zero flicker.
-        </p>
       </div>
     );
   }
