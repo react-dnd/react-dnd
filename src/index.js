fae87995f4d60a49e573549da98c516b6b7d0b55
Dan Abramov <dan.abramov@gmail.com>
2015-05-01 20:23:42 +0300

    Warn that react-dnd has no default export

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 export { default as configureDragDropContext } from './configureDragDropContext';
 export { default as configureDragDropLayer } from './configureDragDropLayer';
 export { default as configureDragDrop } from './configureDragDrop';
+
+if (process.env.NODE_ENV !== 'production') {
+  Object.defineProperty(exports, 'default', {
+    get() {
+      console.error(
+        'React DnD does not provide a default export. ' +
+        'You are probably missing the curly braces in the import statement. ' +
+        'Read more: https://gist.github.com/gaearon/cf692e795a0e960b880e'
+      );
+    }
+  });
+}
