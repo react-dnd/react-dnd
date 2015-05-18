26bc564f827d3e271bf97ae39c93a0379b5d4506
Dan Abramov <dan.abramov@gmail.com>
2015-05-18 18:04:58 +0300

    Add FAQ

--- site/Constants.js
+++ site/Constants.js
@@ -5,6 +5,88 @@ export const Pages = {
   }
 };
 
+export const APIPages = [{
+  title: 'Quick Start',
+  pages: {
+    OVERVIEW: {
+      location: 'docs-overview.html',
+      title: 'Overview'
+    },
+    TUTORIAL: {
+      location: 'docs-tutorial.html',
+      title: 'Tutorial'
+    },
+    TESTING: {
+      location: 'docs-testing.html',
+      title: 'Testing'
+    },
+    FAQ: {
+      location: 'docs-faq.html',
+      title: 'FAQ'
+    }
+  }
+}, {
+  title: 'Top-Level API',
+  pages: {
+    DRAG_SOURCE: {
+      location: 'docs-drag-source.html',
+      title: 'DragSource'
+    },
+    DROP_TARGET: {
+      location: 'docs-drop-target.html',
+      title: 'DropTarget'
+    },
+    DRAG_LAYER: {
+      location: 'docs-drag-layer.html',
+      title: 'DragLayer'
+    },
+    DRAG_DROP_CONTEXT: {
+      location: 'docs-drag-drop-context.html',
+      title: 'DragDropContext'
+    }
+  }
+}, {
+  title: 'Connecting to DOM',
+  pages: {
+    DRAG_SOURCE_CONNECTOR: {
+      location: 'docs-drag-source-connector.html',
+      title: 'DragSourceConnector'
+    },
+    DROP_TARGET_CONNECTOR: {
+      location: 'docs-drop-target-connector.html',
+      title: 'DropTargetConnector'
+    }
+  }
+}, {
+  title: 'Monitoring State',
+  pages: {
+    DRAG_SOURCE_MONITOR: {
+      location: 'docs-drag-source-monitor.html',
+      title: 'DragSourceMonitor'
+    },
+    DROP_TARGET_MONITOR: {
+      location: 'docs-drop-target-monitor.html',
+      title: 'DropTargetMonitor'
+    },
+    DRAG_LAYER_MONITOR: {
+      location: 'docs-drag-layer-monitor.html',
+      title: 'DragLayerMonitor'
+    }
+  }
+}, {
+  title: 'Backends',
+  pages: {
+    HTML5_BACKEND: {
+      location: 'docs-html5-backend.html',
+      title: 'HTML5'
+    },
+    TEST_BACKEND: {
+      location: 'docs-test-backend.html',
+      title: 'Test'
+    }
+  }
+}];
+
 export const ExamplePages = [{
   title: 'Dustbin',
   pages: {
@@ -71,84 +153,6 @@ export const ExamplePages = [{
     CUSTOMIZE_EFFECTS: {
       location: 'examples-customize-effects.html',
       title: 'Drop Effects'
-    }
-  }
-}];
-
-export const APIPages = [{
-  title: 'Quick Start',
-  pages: {
-    OVERVIEW: {
-      location: 'docs-overview.html',
-      title: 'Overview'
-    },
-    TUTORIAL: {
-      location: 'docs-tutorial.html',
-      title: 'Tutorial'
-    },
-    TESTING: {
-      location: 'docs-testing.html',
-      title: 'Testing'
-    }
-  }
-}, {
-  title: 'Top-Level API',
-  pages: {
-    DRAG_SOURCE: {
-      location: 'docs-drag-source.html',
-      title: 'DragSource'
-    },
-    DROP_TARGET: {
-      location: 'docs-drop-target.html',
-      title: 'DropTarget'
-    },
-    DRAG_LAYER: {
-      location: 'docs-drag-layer.html',
-      title: 'DragLayer'
-    },
-    DRAG_DROP_CONTEXT: {
-      location: 'docs-drag-drop-context.html',
-      title: 'DragDropContext'
-    }
-  }
-}, {
-  title: 'Connecting to DOM',
-  pages: {
-    DRAG_SOURCE_CONNECTOR: {
-      location: 'docs-drag-source-connector.html',
-      title: 'DragSourceConnector'
-    },
-    DROP_TARGET_CONNECTOR: {
-      location: 'docs-drop-target-connector.html',
-      title: 'DropTargetConnector'
-    }
-  }
-}, {
-  title: 'Monitoring State',
-  pages: {
-    DRAG_SOURCE_MONITOR: {
-      location: 'docs-drag-source-monitor.html',
-      title: 'DragSourceMonitor'
-    },
-    DROP_TARGET_MONITOR: {
-      location: 'docs-drop-target-monitor.html',
-      title: 'DropTargetMonitor'
-    },
-    DRAG_LAYER_MONITOR: {
-      location: 'docs-drag-layer-monitor.html',
-      title: 'DragLayerMonitor'
-    }
-  }
-}, {
-  title: 'Backends',
-  pages: {
-    HTML5_BACKEND: {
-      location: 'docs-html5-backend.html',
-      title: 'HTML5'
-    },
-    TEST_BACKEND: {
-      location: 'docs-test-backend.html',
-      title: 'Test'
     }
   }
 }];
--- site/IndexPage.js
+++ site/IndexPage.js
@@ -10,6 +10,7 @@ const APIDocs = {
   OVERVIEW: require('../docs/00 Quick Start/Overview.md'),
   TUTORIAL: require('../docs/00 Quick Start/Tutorial.md'),
   TESTING: require('../docs/00 Quick Start/Testing.md'),
+  FAQ: require('../docs/00 Quick Start/FAQ.md'),
   DRAG_SOURCE: require('../docs/01 Top Level API/DragSource.md'),
   DRAG_SOURCE_MONITOR: require('../docs/03 Monitoring State/DragSourceMonitor.md'),
   DRAG_SOURCE_CONNECTOR: require('../docs/02 Connecting to DOM/DragSourceConnector.md'),
