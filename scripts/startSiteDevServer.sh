d03fa8d5e33b3955539505a58462e64a83ed9445
Vivek Kumar Bansal <vkb0310@gmail.com>
2015-06-28 08:17:30 +0530

    fix script to use local webpack

--- scripts/startSiteDevServer.sh
+++ scripts/startSiteDevServer.sh
@@ -4,6 +4,6 @@ PATH=$(npm bin):$PATH
 
 rm -rf ./__site__
 rm -rf ./__site_prerender__
-webpack --config "$PWD/site/webpack-prerender.config.js"
+./node_modules/.bin/webpack --config "$PWD/site/webpack-prerender.config.js"
 ./scripts/buildSiteIndexPages.sh
-webpack-dev-server --config "$PWD/site/webpack-client.config.js" --hot --content-base __site__
+./node_modules/.bin/webpack-dev-server --config "$PWD/site/webpack-client.config.js" --hot --content-base __site__
