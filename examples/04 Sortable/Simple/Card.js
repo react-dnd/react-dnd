58de1ea1f1cc900541a4259a04d75be16e451c0d
Dan Abramov <dan.abramov@gmail.com>
2015-10-12 18:22:19 +0300

    Clarify the mutation

--- examples/04 Sortable/Simple/Card.js
+++ examples/04 Sortable/Simple/Card.js
@@ -58,7 +58,10 @@ const cardTarget = {
     // Time to actually perform the action
     props.moveCard(dragIndex, hoverIndex);
 
-    // Update the monitors item with the new index
+    // Note: we're mutating the monitor item here!
+    // Generally it's better to avoid mutations,
+    // but it's good here for the sake of performance
+    // to avoid expensive index searches.
     monitor.getItem().index = hoverIndex;
   }
 };
