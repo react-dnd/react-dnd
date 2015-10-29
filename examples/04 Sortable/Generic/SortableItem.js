3cac202e03f00250163aaa9350b82d5414716074
Evgueni Naverniouk <evgueni.naverniouk@cumul8.com>
2015-10-29 15:16:52 -0700

    Comment out react-don import

--- examples/04 Sortable/Generic/SortableItem.js
+++ examples/04 Sortable/Generic/SortableItem.js
@@ -1,5 +1,5 @@
 import React, {Component, PropTypes} from 'react';
-import {findDOMNode} from 'react-dom';
+//import {findDOMNode} from 'react-dom';
 import {DragSource, DropTarget} from 'react-dnd';
 
 const sortableSource = {
