6d06642186824255a5b6f499ec10f3b3eb4dc4c3
Dan Abramov <dan.abramov@gmail.com>
2016-02-14 02:17:19 +0000

    Clean up subscriptions before mounting

--- src/createTargetConnector.js
+++ src/createTargetConnector.js
@@ -1,7 +1,55 @@
+import wrapConnectorHooks from './wrapConnectorHooks';
+import areOptionsEqual from './areOptionsEqual';
+
 export default function createTargetConnector(backend) {
-  return {
-    dropTarget: function connectDropTarget(...args) {
-      return backend.connectDropTarget(...args);
+  let currentHandlerId;
+
+  let currentDropTargetNode;
+  let currentDropTargetOptions;
+  let disconnectCurrentDropTarget;
+
+  function reconnectDropTarget() {
+    if (disconnectCurrentDropTarget) {
+      disconnectCurrentDropTarget();
+      disconnectCurrentDropTarget = null;
     }
+
+    if (currentHandlerId && currentDropTargetNode) {
+      disconnectCurrentDropTarget = backend.connectDropTarget(
+        currentHandlerId,
+        currentDropTargetNode,
+        currentDropTargetOptions
+      );
+    }
+  }
+
+  function receiveHandlerId(handlerId) {
+    if (handlerId === currentHandlerId) {
+      return;
+    }
+
+    currentHandlerId = handlerId;
+    reconnectDropTarget();
+  }
+
+  const hooks = wrapConnectorHooks({
+    dropTarget: function connectDropTarget(node, options) {
+      if (
+        node === currentDropTargetNode &&
+        areOptionsEqual(options, currentDropTargetOptions)
+      ) {
+        return;
+      }
+
+      currentDropTargetNode = node;
+      currentDropTargetOptions = options;
+
+      reconnectDropTarget();
+    }
+  });
+
+  return {
+    receiveHandlerId,
+    hooks
   };
 }
