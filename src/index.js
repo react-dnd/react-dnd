ed565cee9160d80afbb5a8bcde576d3d95fd7c28
Primoz Susa <primoz.susa@gmail.com>
2016-03-13 01:46:22 +0100

    Remove development message React DnD does not provide a default export. closes #408

--- src/index.js
+++ src/index.js
@@ -2,15 +2,3 @@ export { default as DragDropContext } from './DragDropContext';
 export { default as DragLayer } from './DragLayer';
 export { default as DragSource } from './DragSource';
 export { default as DropTarget } from './DropTarget';
-
-if (process.env.NODE_ENV !== 'production') {
-  Object.defineProperty(exports, 'default', {
-    get() {
-      console.error( // eslint-disable-line no-console
-        'React DnD does not provide a default export. ' +
-        'You are probably missing the curly braces in the import statement. ' +
-        'Read more: http://gaearon.github.io/react-dnd/docs-troubleshooting.html#react-dnd-does-not-provide-a-default-export'
-      );
-    }
-  });
-}
