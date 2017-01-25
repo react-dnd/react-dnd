4e0123ffee8456d27450bb8249c73ff6fdd27909
Chris Trevino <darthtrevino@gmail.com>
2017-01-24 17:41:21 -0800

    Update dependencies

--- examples/01 Dustbin/Single Target/__tests__/Box-test.js
+++ examples/01 Dustbin/Single Target/__tests__/Box-test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import TestUtils from 'react/lib/ReactTestUtils';
+import TestUtils from 'react-addons-test-utils';
 import expect from 'expect';
 import wrapInTestContext from '../../../shared/wrapInTestContext';
 import Box from '../Box';
