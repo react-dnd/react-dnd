8e09aa32c27a5995a08d67a9e5d36e92122f993a
Chris Trevino <darthtrevino@users.noreply.github.com>
2017-01-25 16:26:32 -0800

    Adding a React Component for establishing Context via React Trees  [v2] (#632)
    * Add DragDropContextProvider React Class
    
    * Correct PropTypes, inject document via ContextType
    
    * Remave getDocument method, get window directly from context instead of through document
    
    * Remove react-dom include
    
    * Avoid using variable names that shadow browser globals
    
    * Update Documentation with Simple iframe Example
    
    * Add some more detail to the iframe exmaple
    
    * Correct linting issue
    
    * Update the container in the iframe example to reference components from the simple example
    
    * Add documentation to the DragDropContextProvider

--- examples/01 Dustbin/Single Target/Container.js
+++ examples/01 Dustbin/Single Target/Container.js
@@ -1,23 +1,24 @@
 import React, { Component } from 'react';
-import { DragDropContext } from 'react-dnd';
+import { DragDropContextProvider } from 'react-dnd';
 import HTML5Backend from 'react-dnd-html5-backend';
 import Dustbin from './Dustbin';
 import Box from './Box';
 
-@DragDropContext(HTML5Backend)
 export default class Container extends Component {
   render() {
     return (
-      <div>
-        <div style={{ overflow: 'hidden', clear: 'both' }}>
-          <Dustbin />
+      <DragDropContextProvider backend={HTML5Backend}>
+        <div>
+          <div style={{ overflow: 'hidden', clear: 'both' }}>
+            <Dustbin />
+          </div>
+          <div style={{ overflow: 'hidden', clear: 'both' }}>
+            <Box name="Glass" />
+            <Box name="Banana" />
+            <Box name="Paper" />
+          </div>
         </div>
-        <div style={{ overflow: 'hidden', clear: 'both' }}>
-          <Box name="Glass" />
-          <Box name="Banana" />
-          <Box name="Paper" />
-        </div>
-      </div>
+      </DragDropContextProvider>
     );
   }
 }
