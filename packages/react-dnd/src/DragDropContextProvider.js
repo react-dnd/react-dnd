bc822eb778cf037fb783405ad320eb6153b961c9
Chris Trevino <darthtrevino@gmail.com>
2017-10-07 10:04:42 -0700

    Format merge into the DragDropContextProvider

--- packages/react-dnd/src/DragDropContextProvider.js
+++ packages/react-dnd/src/DragDropContextProvider.js
@@ -29,40 +29,47 @@ export default class DragDropContextProvider extends Component {
 		window: PropTypes.object,
 	}
 
-  constructor(props, context) {
-    super(props, context)
+	constructor(props, context) {
+		super(props, context)
 
-    /**
+		/**
      * This property determines which window global to use for creating the DragDropManager.
      * If a window has been injected explicitly via props, that is used first. If it is available
      * as a context value, then use that, otherwise use the browser global.
      */
-    const getWindow = () => {
-      if (props && props.window) {
-        return props.window
-      } else if (context && context.window) {
-        return context.window
-      } else if (typeof window !== 'undefined') {
-        return window
-      }
-      return undefined
-    };
+		const getWindow = () => {
+			if (props && props.window) {
+				return props.window
+			} else if (context && context.window) {
+				return context.window
+			} else if (typeof window !== 'undefined') {
+				return window
+			}
+			return undefined
+		}
 
-    this.backend = unpackBackendForEs5Users(props.backend)
-    this.childContext = createChildContext(this.backend, { window: getWindow() })
-  }
+		this.backend = unpackBackendForEs5Users(props.backend)
+		this.childContext = createChildContext(this.backend, {
+			window: getWindow(),
+		})
+	}
 
-  componentWillReceiveProps(nextProps) {
-    if (nextProps.backend !== this.props.backend || nextProps.window !== this.props.window) {
-      throw new Error('DragDropContextProvider backend and window props must not change.')
-    }
-  }
+	componentWillReceiveProps(nextProps) {
+		if (
+			nextProps.backend !== this.props.backend ||
+			nextProps.window !== this.props.window
+		) {
+			throw new Error(
+				'DragDropContextProvider backend and window props must not change.',
+			)
+		}
+	}
 
-  getChildContext() {
-    return this.childContext
-  }
+	getChildContext() {
+		return this.childContext
+	}
 
-  render() {
-    return Children.only(this.props.children)
-  }
+	render() {
+		return Children.only(this.props.children)
+	}
 }
