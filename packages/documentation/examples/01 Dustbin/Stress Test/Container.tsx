9877380e397dec227280a5cc7b67dca13c2258a0
Chris Trevino <chtrevin@microsoft.com>
2018-06-13 14:09:16 -0700

    Run 'build' as part of the test script; fix issues in the docs

--- packages/documentation/examples/01 Dustbin/Stress Test/Container.tsx
+++ packages/documentation/examples/01 Dustbin/Stress Test/Container.tsx
@@ -2,11 +2,11 @@
 import * as React from 'react'
 import { DragDropContext } from 'react-dnd'
 import HTML5Backend, { NativeTypes } from 'react-dnd-html5-backend'
-import shuffle from 'lodash/shuffle'
 import Dustbin from './Dustbin'
 import Box from './Box'
 import ItemTypes from './ItemTypes'
 const update = require('immutability-helper')
+const shuffle = require('lodash/shuffle')
 
 export interface SourceBox {
 	name: string
