90ca9981fc42c7647f88de7e000aa364649b0de0
Chris Trevino <chtrevin@microsoft.com>
2018-06-18 16:16:52 -0700

    Clean up types in the examples

--- packages/documentation/examples/02 Drag Around/Naive/Container.tsx
+++ packages/documentation/examples/02 Drag Around/Naive/Container.tsx
@@ -45,7 +45,7 @@ export interface ContainerState {
 	boxes: { [key: string]: { top: number; left: number; title: string } }
 }
 
-@DragDropContext<ContainerProps, Container>(HTML5Backend)
+@DragDropContext<ContainerProps>(HTML5Backend)
 @DropTarget(ItemTypes.BOX, boxTarget, (connect: any) => ({
 	connectDropTarget: connect.dropTarget(),
 }))
