9e8c05e4c36b1e1f8d107ec68092ef85554bd69b
Chris Trevino <darthtrevino@gmail.com>
2018-11-05 22:19:39 -0800

    docs: updates for static site building

--- packages/documentation/src/pages/docs.tsx
+++ packages/documentation/src/pages/docs.tsx
@@ -3,7 +3,9 @@ import Layout from '../components/layout'
 import { navigate } from 'gatsby'
 
 const IndexPage: React.SFC = () => {
-	navigate('/about')
+	if (typeof window !== 'undefined') {
+		navigate('/about')
+	}
 	return <Layout hideSidebar={true}>{null}</Layout>
 }
 
