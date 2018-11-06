e4787282914d40e83a6b3c2ae245e666306c3149
Chris Trevino <darthtrevino@gmail.com>
2018-11-05 21:34:50 -0800

    docs: add favicon, missing libs

--- packages/documentation/gatsby-config.js
+++ packages/documentation/gatsby-config.js
@@ -24,6 +24,7 @@ module.exports = {
 		'gatsby-plugin-typescript',
 		'gatsby-plugin-react-helmet',
 		'gatsby-plugin-styled-components',
+		'gatsby-plugin-favicon',
 
 		// Handle Markdown Content
 		{
