f61edf9e83d7f8e8858eaa0c000bdf89cbe34ac3
Chris Trevino <darthtrevino@gmail.com>
2018-11-07 10:54:24 -0800

    docs: correct state issue in sortable-stress-test

--- packages/documentation-examples/src/04 Sortable/Stress Test/index.tsx
+++ packages/documentation-examples/src/04 Sortable/Stress Test/index.tsx
@@ -10,7 +10,8 @@ export default class SortableStressTest extends React.Component<
 	SortableStressTestState
 > {
 	// Avoid rendering on server because the big data list is generated
-	// public state = { shouldRender: false }
+	public state = { shouldRender: false }
+
 	public componentDidMount() {
 		// Won't fire on server.
 		this.setState({ shouldRender: true })
