60eff8f597e680a74201080aef5ef5d5c22d5d2e
Chris Trevino <darthtrevino@gmail.com>
2018-11-08 22:58:31 -0800

    refactor: split up the dragdrop actions into separate files

---
+++ packages/dnd-core/src/actions/dragDrop/types.ts
@@ -0,0 +1,6 @@
+export const INIT_COORDS = 'dnd-core/INIT_COORDS'
+export const BEGIN_DRAG = 'dnd-core/BEGIN_DRAG'
+export const PUBLISH_DRAG_SOURCE = 'dnd-core/PUBLISH_DRAG_SOURCE'
+export const HOVER = 'dnd-core/HOVER'
+export const DROP = 'dnd-core/DROP'
+export const END_DRAG = 'dnd-core/END_DRAG'
