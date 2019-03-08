44350abdf556bede07ba5fbfcb3d244e7bfb20ec
Chris Trevino <darthtrevino@gmail.com>
2019-03-07 20:05:42 -0800

    docs: move dustbin box to hooks

--- packages/examples-hooks/src/01 Dustbin/Copy or Move/Box.tsx
+++ packages/examples-hooks/src/01 Dustbin/Copy or Move/Box.tsx
@@ -1,11 +1,9 @@
-import React from 'react'
-import {
-	DragSource,
-	ConnectDragSource,
-	DragSourceConnector,
-	DragSourceMonitor,
-} from 'react-dnd'
+import React, { useRef } from 'react'
 import ItemTypes from '../Single Target/ItemTypes'
+import { __EXPERIMENTAL_DND_HOOKS_THAT_MAY_CHANGE_AND_BREAK_MY_BUILD__ } from 'react-dnd'
+const {
+	useDrag,
+} = __EXPERIMENTAL_DND_HOOKS_THAT_MAY_CHANGE_AND_BREAK_MY_BUILD__
 
 const style: React.CSSProperties = {
 	border: '1px dashed gray',
@@ -16,61 +14,49 @@ const style: React.CSSProperties = {
 	float: 'left',
 }
 
-const boxSource = {
-	beginDrag(props: BoxProps) {
-		return {
-			name: props.name,
-		}
-	},
-
-	endDrag(props: BoxProps, monitor: DragSourceMonitor) {
-		const item = monitor.getItem()
-		const dropResult = monitor.getDropResult()
-
-		if (dropResult) {
-			let alertMessage = ''
-			const isDropAllowed =
-				dropResult.allowedDropEffect === 'any' ||
-				dropResult.allowedDropEffect === dropResult.dropEffect
-
-			if (isDropAllowed) {
-				const isCopyAction = dropResult.dropEffect === 'copy'
-				const actionName = isCopyAction ? 'copied' : 'moved'
-				alertMessage = `You ${actionName} ${item.name} into ${dropResult.name}!`
-			} else {
-				alertMessage = `You cannot ${dropResult.dropEffect} an item into the ${
-					dropResult.name
-				}`
-			}
-			alert(alertMessage)
-		}
-	},
-}
-
 export interface BoxProps {
 	name: string
 }
 
-interface BoxCollectedProps {
-	isDragging: boolean
-	connectDragSource: ConnectDragSource
+const Box: React.FC<BoxProps> = ({ name }) => {
+	const ref = useRef(null)
+	const { opacity } = useDrag({
+		ref,
+		type: ItemTypes.BOX,
+		begin: () => ({ name }),
+		end(monitor) {
+			const item = monitor.getItem()
+			const dropResult = monitor.getDropResult()
+
+			if (dropResult) {
+				let alertMessage = ''
+				const isDropAllowed =
+					dropResult.allowedDropEffect === 'any' ||
+					dropResult.allowedDropEffect === dropResult.dropEffect
+
+				if (isDropAllowed) {
+					const isCopyAction = dropResult.dropEffect === 'copy'
+					const actionName = isCopyAction ? 'copied' : 'moved'
+					alertMessage = `You ${actionName} ${item.name} into ${
+						dropResult.name
+					}!`
+				} else {
+					alertMessage = `You cannot ${
+						dropResult.dropEffect
+					} an item into the ${dropResult.name}`
+				}
+				alert(alertMessage)
+			}
+		},
+		collect: monitor => ({
+			opacity: monitor.isDragging() ? 0.4 : 1,
+		}),
+	})
+
+	return (
+		<div ref={ref} style={{ ...style, opacity }}>
+			{name}
+		</div>
+	)
 }
-
-class Box extends React.Component<BoxProps & BoxCollectedProps> {
-	public render() {
-		const { isDragging, connectDragSource } = this.props
-		const { name } = this.props
-		const opacity = isDragging ? 0.4 : 1
-
-		return connectDragSource(<div style={{ ...style, opacity }}>{name}</div>)
-	}
-}
-
-export default DragSource(
-	ItemTypes.BOX,
-	boxSource,
-	(connect: DragSourceConnector, monitor: DragSourceMonitor) => ({
-		connectDragSource: connect.dragSource(),
-		isDragging: monitor.isDragging(),
-	}),
-)(Box)
+export default Box
