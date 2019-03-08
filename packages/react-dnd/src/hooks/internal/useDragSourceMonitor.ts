b7614867034541b629f0cf4eda1c1e298a3c1696
Chris Trevino <darthtrevino@users.noreply.github.com>
2019-03-08 10:31:11 -0800

    fix: update the hooks to pass the right monitor impl to specs (#1259)

--- packages/react-dnd/src/hooks/internal/useDragSourceMonitor.ts
+++ packages/react-dnd/src/hooks/internal/useDragSourceMonitor.ts
@@ -1,21 +1,63 @@
-import { useMemo, useEffect } from 'react'
-import registerSource from '../../registerSource'
-import { DragDropManager, SourceType, DragSource } from 'dnd-core'
+import { useMemo, useEffect, useRef } from 'react'
+import { DragSource, DragDropManager } from 'dnd-core'
+import { DragSourceHookSpec, DragSourceMonitor } from '../../interfaces'
 import DragSourceMonitorImpl from '../../DragSourceMonitorImpl'
+import registerSource from '../../registerSource'
 
-export function useDragSourceMonitor<Context>(
-	type: SourceType,
-	source: DragSource,
-	manager: DragDropManager<Context>,
-) {
+export function useDragSourceMonitor<DragObject, CustomProps>(
+	manager: DragDropManager<any>,
+	sourceSpec: DragSourceHookSpec<DragObject, CustomProps>,
+): DragSourceMonitor {
+	const sourceSpecRef = useRef(sourceSpec)
+
+	useEffect(() => {
+		sourceSpecRef.current = sourceSpec
+	})
+
 	const monitor = useMemo(() => new DragSourceMonitorImpl(manager), [manager])
 	useEffect(
 		function registerSourceWithMonitor() {
-			const { handlerId, unregister } = registerSource(type, source, manager)
+			const { handlerId, unregister } = registerSource(
+				sourceSpec.type,
+				handler,
+				manager,
+			)
 			monitor.receiveHandlerId(handlerId)
 			return unregister
 		},
 		[monitor],
+	)
+
+	// Can't use createSourceFactory, as semantics are different
+	const handler = useMemo(
+		() =>
+			({
+				beginDrag() {
+					const { begin } = sourceSpecRef.current
+					if (begin) {
+						return begin(monitor)
+					} else {
+						return {}
+					}
+				},
+				canDrag() {
+					const { canDrag } = sourceSpecRef.current
+					return canDrag ? canDrag(monitor) : true
+				},
+				isDragging(globalMonitor, target) {
+					const { isDragging } = sourceSpecRef.current
+					return isDragging
+						? isDragging(monitor)
+						: target === globalMonitor.getSourceId()
+				},
+				endDrag() {
+					const { end } = sourceSpecRef.current
+					if (end) {
+						end(monitor)
+					}
+				},
+			} as DragSource),
+		[],
 	)
 
 	return monitor
