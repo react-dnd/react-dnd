047938321fcd6edfbb8a378e86bf3b2653d953c8
Chris Trevino <darthtrevino@gmail.com>
2019-03-07 23:27:46 -0800

    docs: fix custom drag layer example

--- packages/examples-hooks/src/02 Drag Around/Custom Drag Layer/DraggableBox.tsx
+++ packages/examples-hooks/src/02 Drag Around/Custom Drag Layer/DraggableBox.tsx
@@ -4,6 +4,7 @@ import {
 	DragSourceMonitor,
 } from 'react-dnd'
 import ItemTypes from './ItemTypes'
+import { getEmptyImage } from 'react-dnd-html5-backend'
 import Box from './Box'
 const {
 	useDrag,
@@ -39,25 +40,19 @@ const DraggableBox: React.FC<DraggableBoxProps> = props => {
 	const { isDragging } = useDrag({
 		ref,
 		type: ItemTypes.BOX,
+		// Use empty image as a drag preview so browsers don't draw it
+		// and we can draw whatever we want on the custom drag layer instead.
+		preview: getEmptyImage(),
+		previewOptions: {
+			// IE fallback: specify that we'd rather screenshot the node
+			// when it already knows it's being dragged so we can hide it with CSS.
+			captureDraggingState: true,
+		},
 		begin: () => ({ id, title, left, top }),
 		collect: (monitor: DragSourceMonitor) => ({
 			isDragging: monitor.isDragging(),
 		}),
 	})
-	/*
-	TODO: figure out what to do here
-	React.useEffect(() => {
-		if (connectDragPreview) {
-			// Use empty image as a drag preview so browsers don't draw it
-			// and we can draw whatever we want on the custom drag layer instead.
-			connectDragPreview(getEmptyImage(), {
-				// IE fallback: specify that we'd rather screenshot the node
-				// when it already knows it's being dragged so we can hide it with CSS.
-				captureDraggingState: true,
-			})
-		}
-	})
-	*/
 
 	return (
 		<div ref={ref} style={getStyles(props, isDragging)}>
