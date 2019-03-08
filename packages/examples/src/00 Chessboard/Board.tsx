80feae4090d239d3af98eea748301c933de35d4e
Chris Trevino <darthtrevino@gmail.com>
2019-03-07 19:05:05 -0800

    fix: ts issues

--- packages/examples/src/00 Chessboard/Board.tsx
+++ packages/examples/src/00 Chessboard/Board.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import { BoardSquare } from './BoardSquare'
-import { Knight } from './Knight'
+import BoardSquare from './BoardSquare'
+import Knight from './Knight'
 
 export interface BoardProps {
 	knightPosition: [number, number]
--- packages/examples/src/02 Drag Around/Custom Drag Layer/Container.tsx
+++ packages/examples/src/02 Drag Around/Custom Drag Layer/Container.tsx
@@ -1,3 +1,4 @@
+declare var require: any
 import * as React from 'react'
 import {
 	DropTarget,
