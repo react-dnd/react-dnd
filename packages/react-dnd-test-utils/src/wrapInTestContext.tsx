34b87b18708b2dc49974a5dbfbfc3ec8e09c66f2
Chris Trevino <darthtrevino@gmail.com>
2019-03-22 10:05:54 -0700

    test: fix tests

--- packages/react-dnd-test-utils/src/wrapInTestContext.tsx
+++ packages/react-dnd-test-utils/src/wrapInTestContext.tsx
@@ -2,7 +2,7 @@ import * as React from 'react'
 import TestBackend from 'react-dnd-test-backend'
 import { DragDropContext } from 'react-dnd'
 
-export default function wrapInTestContext(DecoratedComponent: any): any {
+export function wrapInTestContext(DecoratedComponent: any): any {
 	const TestStub = (props: any) => <DecoratedComponent {...props} />
 	return DragDropContext(TestBackend)(TestStub)
 }
