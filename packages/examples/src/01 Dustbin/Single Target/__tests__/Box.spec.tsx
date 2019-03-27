4a2a693d0fdabcc6dd563b1979bfa2b856a299da
Chris Trevino <darthtrevino@gmail.com>
2019-03-26 21:37:53 -0700

    test: setup enzyme, re-enable box tests

--- packages/examples/src/01 Dustbin/Single Target/__tests__/Box.spec.tsx
+++ packages/examples/src/01 Dustbin/Single Target/__tests__/Box.spec.tsx
@@ -2,52 +2,38 @@ import React from 'react'
 import * as TestUtils from 'react-dom/test-utils'
 import Box from '../Box'
 import { wrapInTestContext } from 'react-dnd-test-utils'
+import { mount } from 'enzyme'
 
-class Wrapper extends React.Component {
-	public render() {
-		return this.props.children
-	}
-}
 describe('Box', () => {
 	// TODO: test utils are acting wonking with function components.
 	// take a pass at making the tests behave better
-	it.skip('can be tested independently', () => {
+	it('can be tested independently', () => {
 		// Obtain the reference to the component before React DnD wrapping
 		const OriginalBox = Box.DecoratedComponent
 
 		// Stub the React DnD connector functions with an identity function
 		const identity = (x: any) => x
 
-		const TestCase: React.FC = () => (
-			<Wrapper>
-				<OriginalBox
-					name="test"
-					connectDragSource={identity}
-					isDragging={false}
-				/>
-			</Wrapper>
-		)
-
 		// Render with one set of props and test
-		let root: any = TestUtils.renderIntoDocument(<TestCase />)
-		let div: HTMLDivElement = TestUtils.findRenderedDOMComponentWithTag(
-			root,
-			'div,',
-		) as HTMLDivElement
+		let mounted = mount(
+			<OriginalBox
+				name="test"
+				connectDragSource={identity}
+				isDragging={false}
+			/>,
+		)
+		let div = mounted.getDOMNode() as HTMLDivElement
 		expect(div.style.opacity).toEqual('1')
 
 		// Render with another set of props and test
-		root = TestUtils.renderIntoDocument(
+		mounted = mount(
 			<OriginalBox
 				name="test"
 				connectDragSource={identity}
 				isDragging={true}
 			/>,
 		)
-		div = TestUtils.findRenderedDOMComponentWithTag(
-			root,
-			'div',
-		) as HTMLDivElement
+		div = mounted.getDOMNode() as HTMLDivElement
 		expect(div.style.opacity).toEqual('0.4')
 	})
 
