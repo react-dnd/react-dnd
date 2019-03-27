7b413373617dfe11a89e0a21a27c310ecc06f19d
Chris Trevino <darthtrevino@gmail.com>
2019-03-27 09:48:25 -0700

    docs: add react-codesandboxer to examples

--- packages/examples/src/05 Customize/Handles and Previews/index.tsx
+++ packages/examples/src/05 Customize/Handles and Previews/index.tsx
@@ -1,9 +1,20 @@
 import React from 'react'
 import BoxWithImage from './BoxWithImage'
 import BoxWithHandle from './BoxWithHandle'
+const CodeSandboxer = require('react-codesandboxer')
 
 const Container: React.FC = () => (
 	<div>
+		<CodeSandboxer
+			examplePath="examples_js/05%20Customize/Handles%20and%20Previews/index.js"
+			gitInfo={{
+				account: 'react-dnd',
+				repository: 'react-dnd',
+				host: 'github',
+			}}
+		>
+			{() => <button type="submit">Upload to CodeSandbox</button>}
+		</CodeSandboxer>
 		<div style={{ marginTop: '1.5rem' }}>
 			<BoxWithHandle />
 			<BoxWithImage />
