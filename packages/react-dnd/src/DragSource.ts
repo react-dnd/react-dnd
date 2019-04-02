ae7ffe21940b4c7830837edaf7b3e214178afbaf
Chris Trevino <darthtrevino@users.noreply.github.com>
2019-04-02 16:40:11 -0700

    Correct links to documentation in error messages (#1314)
    * fix: correct doc links in error messages
    
    * fix: update documentation links in tsx files

--- packages/react-dnd/src/DragSource.ts
+++ packages/react-dnd/src/DragSource.ts
@@ -47,7 +47,7 @@ export default function DragSource<
 			'Expected "type" provided as the first argument to DragSource to be ' +
 				'a string, or a function that returns a string given the current props. ' +
 				'Instead, received %s. ' +
-				'Read more: http://react-dnd.github.io/react-dnd/docs-drag-source.html',
+				'Read more: http://react-dnd.github.io/react-dnd/docs/api/drag-source',
 			type,
 		)
 		getType = () => type
@@ -56,7 +56,7 @@ export default function DragSource<
 		isPlainObject(spec),
 		'Expected "spec" provided as the second argument to DragSource to be ' +
 			'a plain object. Instead, received %s. ' +
-			'Read more: http://react-dnd.github.io/react-dnd/docs-drag-source.html',
+			'Read more: http://react-dnd.github.io/react-dnd/docs/api/drag-source',
 		spec,
 	)
 	const createSource = createSourceFactory(spec)
@@ -65,7 +65,7 @@ export default function DragSource<
 		'Expected "collect" provided as the third argument to DragSource to be ' +
 			'a function that returns a plain object of props to inject. ' +
 			'Instead, received %s. ' +
-			'Read more: http://react-dnd.github.io/react-dnd/docs-drag-source.html',
+			'Read more: http://react-dnd.github.io/react-dnd/docs/api/drag-source',
 		collect,
 	)
 	invariant(
@@ -73,7 +73,7 @@ export default function DragSource<
 		'Expected "options" provided as the fourth argument to DragSource to be ' +
 			'a plain object when specified. ' +
 			'Instead, received %s. ' +
-			'Read more: http://react-dnd.github.io/react-dnd/docs-drag-source.html',
+			'Read more: http://react-dnd.github.io/react-dnd/docs/api/drag-source',
 		collect,
 	)
 
