dfc2d4b9e6237d4b368138d0927a96efcb1e834e
Chris Trevino <darthtrevino@users.noreply.github.com>
2019-05-29 10:05:50 -0700

    fix: update the isRef check to only check for the existence of a 'current' property (#1359)

--- packages/react-dnd/src/utils/isRef.ts
+++ packages/react-dnd/src/utils/isRef.ts
@@ -3,9 +3,7 @@ export interface Ref<T> {
 }
 
 export function isRef(obj: any) {
-	if (obj !== null && typeof obj === 'object') {
-		const keys = Object.keys(obj)
-		return keys.length === 1 && keys[0] === 'current'
-	}
-	return false
+	return (
+		obj !== null && typeof obj === 'object' && obj.hasOwnProperty('current')
+	)
 }
