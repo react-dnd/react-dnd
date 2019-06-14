1da4e7f6d8daaadc5592c1ff6a503e06089ae2c4
Chris Trevino <darthtrevino@gmail.com>
2019-06-14 15:38:31 -0700

    docs: udate DndContext documentation

--- packages/documentation/write_sample_boilerplate.js
+++ packages/documentation/write_sample_boilerplate.js
@@ -6,15 +6,15 @@ const APP_FILE_CONTENT = `
 	import React from 'react'
 	import ReactDOM from 'react-dom'
 	import Example from './example'
-	import { DragDropContextProvider } from 'react-dnd'
+	import { DndProvider } from 'react-dnd'
 	import HTML5Backend from 'react-dnd-html5-backend'
 	
 	function App() {
 		return (
 			<div className="App">
-				<DragDropContextProvider backend={HTML5Backend}>
+				<DndProvider backend={HTML5Backend}>
 					<Example />
-				</DragDropContextProvider>
+				</DndProvider>
 			</div>
 		)
 	}
