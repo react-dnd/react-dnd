c7253d7bc447b9068c656cd7cb740458209e7337
Chris Trevino <darthtrevino@users.noreply.github.com>
2019-06-21 12:03:09 -0700

    Use a Singleton DndContext instance when rerendering DndProvider (#1422)
    * fix: use a singleton DndContext instance to multiple-backend issues when DndProvider rerenders
    
    * docs: add editorconfig to see if github changes tab width when it's present

--- packages/core/react-dnd/src/common/DndProvider.tsx
+++ packages/core/react-dnd/src/common/DndProvider.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react'
+import { memo } from 'react'
 import { DragDropManager, BackendFactory } from 'dnd-core'
 import { DndContext, createDndContext } from './DndContext'
 
@@ -15,15 +16,28 @@ export type DndProviderProps<BackendContext> =
 /**
  * A React component that provides the React-DnD context
  */
-export const DndProvider: React.FC<DndProviderProps<any>> = ({
-	children,
-	...props
-}) => {
-	const contextValue =
-		'manager' in props
-			? { dragDropManager: props.manager }
-			: createDndContext(props.backend, props.context, props.debugMode)
-	return (
-		<DndContext.Provider value={contextValue}>{children}</DndContext.Provider>
-	)
+export const DndProvider: React.FC<DndProviderProps<any>> = memo(
+	({ children, ...props }) => {
+		const context =
+			'manager' in props
+				? { dragDropManager: props.manager }
+				: createSingletonDndContext(
+						props.backend,
+						props.context,
+						props.debugMode,
+				  )
+		return <DndContext.Provider value={context}>{children}</DndContext.Provider>
+	},
+)
+
+let SINGLETON_DND_CONTEXT: DndContext<any> | undefined
+function createSingletonDndContext<BackendContext>(
+	backend: BackendFactory,
+	context?: BackendContext,
+	debugMode?: boolean,
+) {
+	if (!SINGLETON_DND_CONTEXT) {
+		SINGLETON_DND_CONTEXT = createDndContext(backend, context, debugMode)
+	}
+	return SINGLETON_DND_CONTEXT
 }
