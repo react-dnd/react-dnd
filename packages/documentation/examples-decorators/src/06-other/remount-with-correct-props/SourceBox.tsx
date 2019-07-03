f9d67b3bb35b849abb9f2d822ec74524aa1a88a2
Chris Trevino <darthtrevino@gmail.com>
2019-07-03 11:02:18 -0700

    docs: add test case based on codesandbox sample in reported issue

---
+++ packages/documentation/examples-decorators/src/06-other/remount-with-correct-props/SourceBox.tsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import { DragSource, ConnectDragSource } from 'react-dnd'
+import ItemTypes from './ItemTypes'
+
+const getStyle = (isDragging: boolean) => ({
+	display: 'inline-block',
+	padding: '0.5rem 1rem',
+	cursor: 'pointer',
+	border: '1px dashed gray',
+	backgroundColor: isDragging ? 'red' : undefined,
+	opacity: isDragging ? 0.4 : 1,
+})
+
+export interface SourceBoxProps {
+	id: string
+	isDragging: boolean
+	connectDragSource: ConnectDragSource
+	onBeginDrag: () => void
+	onEndDrag: () => void
+}
+const SourceBox: React.FC<SourceBoxProps> = ({
+	isDragging,
+	connectDragSource,
+}) => {
+	return connectDragSource(<div style={getStyle(isDragging)}>Drag me</div>)
+}
+
+export default DragSource(
+	ItemTypes.BOX,
+	{
+		beginDrag: (props: SourceBoxProps) => {
+			props.onBeginDrag()
+			return { id: props.id }
+		},
+		endDrag: (props: SourceBoxProps) => {
+			props.onEndDrag()
+		},
+		isDragging: (props: SourceBoxProps, monitor) =>
+			monitor.getItem().id === props.id,
+	},
+	(connect, monitor) => ({
+		connectDragSource: connect.dragSource(),
+		isDragging: monitor.isDragging(),
+	}),
+)(SourceBox)
---
+++ packages/documentation/examples-hooks/src/06-other/remount-with-correct-props/SourceBox.tsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import { useDrag } from 'react-dnd'
+import ItemTypes from './ItemTypes'
+
+const getStyle = (isDragging: boolean) => ({
+	display: 'inline-block',
+	padding: '0.5rem 1rem',
+	cursor: 'pointer',
+	border: '1px dashed gray',
+	backgroundColor: isDragging ? 'red' : undefined,
+	opacity: isDragging ? 0.4 : 1,
+})
+
+export interface SourceBoxProps {
+	id: string
+	onBeginDrag: () => void
+	onEndDrag: () => void
+}
+export const SourceBox: React.FC<SourceBoxProps> = ({
+	id,
+	onBeginDrag,
+	onEndDrag,
+}) => {
+	const [{ isDragging }, drag] = useDrag({
+		item: { type: ItemTypes.BOX, id },
+		isDragging: monitor => monitor.getItem().id === id,
+		collect: monitor => ({ isDragging: monitor.isDragging() }),
+		begin: onBeginDrag,
+		end: onEndDrag,
+	})
+
+	return (
+		<div ref={drag} style={getStyle(isDragging)}>
+			Drag me
+		</div>
+	)
+}
