ec79db5cda11ead3923403fe8e11ed3b0198ac9f
Chris Trevino <darthtrevino@gmail.com>
2019-07-10 12:20:36 -0700

    build: use babel for transpilation, use typescript only to check and emit typings

---
+++ babel.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+	presets: [
+		'@babel/preset-typescript',
+		[
+			'@babel/preset-env',
+			{
+				modules: false,
+				targets: {
+					browsers: ['>0.25%, not dead'],
+				},
+			},
+		],
+	],
+	plugins: [
+		'@babel/proposal-class-properties',
+		'@babel/proposal-object-rest-spread',
+	],
+}
