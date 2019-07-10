b043ee607de9595385110aab86b8bfaeac384c03
Chris Trevino <darthtrevino@gmail.com>
2019-07-10 13:51:18 -0700

    build: turn on no-var-requires linting rule

--- jest/setup-enzyme.js
+++ jest/setup-enzyme.js
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 const Enzyme = require('enzyme')
 const Adapter = require('enzyme-adapter-react-16')
 
--- packages/alternative_builds/umd/webpack.config.js
+++ packages/alternative_builds/umd/webpack.config.js
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 const path = require('path')
 
 function createWebpackConfiguration() {
