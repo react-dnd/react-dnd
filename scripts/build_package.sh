080f1bcc4a4818f898eae94002ec10c66f92f0b0
Chris Trevino <darthtrevino@gmail.com>
2019-07-10 20:43:30 -0700

    build: parallelize the build_package task

--- scripts/build_package.sh
+++ scripts/build_package.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-npx babel --config-file=../../../babel.config.js src --ignore="src/**/__tests__/**" --out-dir lib --extensions '.ts,.tsx'
-npx tsc
+npx babel --config-file=../../../babel.config.js src --ignore="src/**/__tests__/**" --out-dir lib --extensions '.ts,.tsx' &
+npx tsc &
+wait
