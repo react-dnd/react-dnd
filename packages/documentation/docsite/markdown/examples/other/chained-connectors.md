2cfe953d81f1e2067335a7f030cc2e1d371cbba1
Chris Trevino <darthtrevino@gmail.com>
2019-08-05 12:58:50 -0700

    docs: add chained-connectors regression test

---
+++ packages/documentation/docsite/markdown/examples/other/chained-connectors.md
@@ -0,0 +1,9 @@
+---
+path: '/examples/other/chained-connectors'
+title: 'Chained Connecors'
+---
+
+Example using chained connector functions.
+
+<view-source name="06-other/chained-connectors" component="other-chained-connectors">
+</view-source>
---
+++ packages/documentation/examples-decorators/src/06-other/chained-connectors/BoxWithHandle.tsx
@@ -0,0 +1,67 @@
+import React from 'react'
+import {
+	DragSource,
+	DropTarget,
+	ConnectDragSource,
+	ConnectDragPreview,
+	ConnectDropTarget,
+} from 'react-dnd'
+import ItemTypes from './ItemTypes'
+
+const style = {
+	border: '1px dashed gray',
+	padding: '0.5rem 1rem',
+	marginBottom: '.5rem',
+	backgroundColor: 'white',
+	width: '20rem',
+}
+const handleStyle = {
+	backgroundColor: 'green',
+	width: '1rem',
+	height: '1rem',
+	display: 'inline-block',
+	marginRight: '0.75rem',
+	cursor: 'move',
+}
+
+export interface BoxWithHandleProps {
+	isDragging: boolean
+	connectDragSource: ConnectDragSource
+	connectDragPreview: ConnectDragPreview
+	connectDropTarget: ConnectDropTarget
+}
+
+const BoxWithHandleRaw: React.FC<BoxWithHandleProps> = ({
+	isDragging,
+	connectDragSource,
+	connectDragPreview,
+	connectDropTarget,
+}: BoxWithHandleProps) => {
+	const opacity = isDragging ? 0.4 : 1
+	return connectDropTarget(
+		connectDragPreview(
+			<div style={{ ...style, opacity }}>
+				{connectDragSource(<div style={handleStyle} />)}
+				Drag me by the handle BAD
+			</div>,
+		),
+	)
+}
+
+export const BoxWithHandle = DragSource(
+	ItemTypes.BOX,
+	{
+		beginDrag: () => ({}),
+	},
+	(connect, monitor) => ({
+		connectDragSource: connect.dragSource(),
+		connectDragPreview: connect.dragPreview(),
+		isDragging: monitor.isDragging(),
+	}),
+)(
+	DropTarget(ItemTypes.BOX, {}, (connect, monitor) => ({
+		connectDropTarget: connect.dropTarget(),
+		isDraggingHover: monitor.isOver({ shallow: true }),
+		isOver: monitor.isOver(),
+	}))(BoxWithHandleRaw),
+)
