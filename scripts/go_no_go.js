9ae0f71228fb24bf103d86d8368ed6716c4bf5fe
Chris Trevino <darthtrevino@gmail.com>
2019-08-05 23:32:21 -0700

    build: add manual go/no-go script to prepublish

---
+++ scripts/go_no_go.js
@@ -0,0 +1,18 @@
+const inquirer = require('inquirer')
+inquirer
+	.prompt([
+		{
+			type: 'input',
+			default: false,
+			name: 'go',
+			message: 'Is everything ready for publication?',
+		},
+	])
+	.then(answers => {
+		if (answers.go === 'true') {
+			console.log('🚀')
+		} else {
+			console.log('💥')
+			process.exit(1)
+		}
+	})
