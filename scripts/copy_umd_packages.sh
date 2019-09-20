cef8988abc57bfbe263dd2df27aa9209c15c4c53
Chris Trevino <darthtrevino@gmail.com>
2019-09-20 10:29:47 -0700

    build: update copy script

--- scripts/copy_umd_packages.sh
+++ scripts/copy_umd_packages.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
-cp packages/core/react-dnd/dist/umd/*.js packages/alternative_builds/umd/dist/ &&
+mkdir -p packages/alternative_builds/umd/dist && 
+cp packages/core/react-dnd/dist/umd/*.js packages/alternative_builds/umd/dist &&
 cp packages/core/html5-backend/dist/umd/*.js packages/alternative_builds/umd/dist && 
 cp packages/core/touch-backend/dist/umd/*.js packages/alternative_builds/umd/dist
