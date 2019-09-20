ed616c4e090f87e21040126f9b8204beb84650da
Chris Trevino <darthtrevino@gmail.com>
2019-09-20 10:07:39 -0700

    build: add a script to copy umd builds into the umd builds package
    This will allow for users to have UMD builds available either from the packages directly or from the react-dnd-umd-builds packag.

---
+++ scripts/copy_umd_packages.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+cp packages/core/react-dnd/dist/umd/*.js packages/alternative_builds/umd/dist/ &&
+cp packages/core/html5-backend/dist/umd/*.js packages/alternative_builds/umd/dist && 
+cp packages/core/touch-backend/dist/umd/*.js packages/alternative_builds/umd/dist
