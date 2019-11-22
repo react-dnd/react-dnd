e92ce90b23e27cff75ddc0865e093d97e3ba5911
Chris Trevino <darthtrevino@gmail.com>
2019-11-22 14:28:33 -0800

    fix: update loadDataTransfer to accept null

--- packages/core/html5-backend/src/NativeDragSources/NativeDragSource.ts
+++ packages/core/html5-backend/src/NativeDragSources/NativeDragSource.ts
@@ -27,7 +27,7 @@ export class NativeDragSource {
 		})
 	}
 
-	public loadDataTransfer(dataTransfer?: DataTransfer) {
+	public loadDataTransfer(dataTransfer: DataTransfer | null | undefined) {
 		if (dataTransfer) {
 			const newProperties: PropertyDescriptorMap = {}
 			Object.keys(this.config.exposeProperties).forEach(property => {
