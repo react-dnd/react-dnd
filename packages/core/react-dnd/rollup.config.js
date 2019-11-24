1863f1139b6bff8cc0868587482657a0579afeff
Chris Trevino <darthtrevino@users.noreply.github.com>
2019-11-24 15:58:09 -0800

    Replace Webpack UMD Build with Rollup (#1621)
    * build(umd): add rollup tooling
    
    * chore(deps): upgrade dependencies:
    
    * chore(deps): update yarn.lock
    
    * build(rollup): define react,react-dom globals
    
    * build(rollup): move rollup config to react-dnd package
    
    Add rollup invocation to build_package.sh
    
    * build(rollup): add configs for backends
    
    * build(bundling): remove webpack steps
    
    * chore(deps): remove webpack from lockfile

---
+++ packages/core/react-dnd/rollup.config.js
@@ -0,0 +1,49 @@
+import path from 'path'
+import babel from 'rollup-plugin-babel'
+import resolve from 'rollup-plugin-node-resolve'
+import commonjs from 'rollup-plugin-commonjs'
+import { terser } from 'rollup-plugin-terser'
+
+export default {
+	input: path.resolve(__dirname, 'lib/index.js'),
+	output: [
+		{
+			name: 'ReactDnD',
+			file: path.resolve(__dirname, 'dist/umd/ReactDnD.js'),
+			format: 'umd',
+		},
+		{
+			name: 'ReactDnD',
+			file: path.resolve(__dirname, 'dist/umd/ReactDnD.min.js'),
+			format: 'umd',
+			plugins: [terser()],
+		},
+	],
+	external: ['react', 'react-dom'],
+	globals: {
+		react: 'React',
+		'react-dom': 'ReactDOM',
+	},
+	plugins: [
+		resolve(),
+		commonjs(),
+		babel({
+			exclude: 'node_modules/**',
+			presets: [
+				[
+					'@babel/env',
+					{
+						modules: 'false',
+						corejs: '3.0.0',
+						targets: {
+							browsers: '> 0.25%, not dead',
+							node: 8,
+							ie: '11',
+						},
+						useBuiltIns: 'usage',
+					},
+				],
+			],
+		}),
+	],
+}
--- packages/core/react-dnd/src/common/DndProvider.tsx
+++ packages/core/react-dnd/src/common/DndProvider.tsx
@@ -1,5 +1,4 @@
-import * as React from 'react'
-import { memo } from 'react'
+import React, { memo } from 'react'
 import { BackendFactory, DragDropManager } from 'dnd-core'
 import { DndContext, createDndContext } from './DndContext'
 
