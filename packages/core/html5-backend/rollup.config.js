f995682a8e2f726e69ad2561a067a6394c89d566
Chris Trevino <darthtrevino@users.noreply.github.com>
2019-11-24 20:29:21 -0800

    Add rollup replacements for process.env.NODE_ENV (#1622)
    * build(rollup): add replacements for process.env.NODE_ENV
    
    * build(rollup): use exotic dep until replace is updated

--- packages/core/html5-backend/rollup.config.js
+++ packages/core/html5-backend/rollup.config.js
@@ -3,6 +3,7 @@ import babel from 'rollup-plugin-babel'
 import resolve from 'rollup-plugin-node-resolve'
 import commonjs from 'rollup-plugin-commonjs'
 import { terser } from 'rollup-plugin-terser'
+import replace from '@rollup/plugin-replace'
 
 export default {
 	input: path.resolve(__dirname, 'lib/index.js'),
@@ -11,12 +12,24 @@ export default {
 			name: 'ReactDnDHTML5Backend',
 			file: path.resolve(__dirname, 'dist/umd/ReactDnDHTML5Backend.js'),
 			format: 'umd',
+			plugins: [
+				replace({
+					values: { 'process.env.NODE_ENV': JSON.stringify('development') },
+					delimiters: ['', ''],
+				}),
+			],
 		},
 		{
 			name: 'ReactDnDHTML5Backend',
 			file: path.resolve(__dirname, 'dist/umd/ReactDnDHTML5Backend.min.js'),
 			format: 'umd',
-			plugins: [terser()],
+			plugins: [
+				terser(),
+				replace({
+					values: { 'process.env.NODE_ENV': JSON.stringify('production') },
+					delimiters: ['', ''],
+				}),
+			],
 		},
 	],
 	external: ['react', 'react-dom', 'react-dnd'],
