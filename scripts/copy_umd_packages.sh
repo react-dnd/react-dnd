479b926822c9d5fd60b52de5100afec50bb52ec8
Chris Trevino <darthtrevino@users.noreply.github.com>
2019-11-25 18:25:51 -0800

    build: update bash scripts to emit errors (#1630)

--- scripts/copy_umd_packages.sh
+++ scripts/copy_umd_packages.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+set -e
 mkdir -p packages/alternative_builds/umd/dist && 
 cp packages/core/react-dnd/dist/umd/*.js packages/alternative_builds/umd/dist &&
 cp packages/core/html5-backend/dist/umd/*.js packages/alternative_builds/umd/dist && 
