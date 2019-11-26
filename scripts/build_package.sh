4634b5671b1f6aa69a43fad7a589ffec1e1e602c
Chris Trevino <darthtrevino@users.noreply.github.com>
2019-11-25 18:25:51 -0800

    build: update bash scripts to emit errors (#1630)

--- scripts/build_package.sh
+++ scripts/build_package.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+set -e
 npx tsc
 npx babel --config-file=../../../babel.config.js lib --ignore="src/**/__tests__/**" --out-dir dist/esm &
 npx babel --config-file=../../../babel.config.cjs.js lib --ignore="src/**/__tests__/**" --out-dir dist/cjs &
--- scripts/watch_package.sh
+++ scripts/watch_package.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
+set -e
 npx babel --config-file=../../../babel.config.js src --ignore="src/**/__tests__/**" --out-dir lib --extensions '.ts,.tsx' -w --verbose
