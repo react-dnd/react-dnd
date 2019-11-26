69a2ebd1eef483fa4f224f44f1b73a1c76f1c6b0
Nathan Shively-Sanders <293473+sandersn@users.noreply.github.com>
2019-11-25 16:07:10 -0800

    Remove allowSyntheticDefaultImports
    1. From base tsconfig.
    2. From react-dnd's tsconfig.
    
    Also fix up the one import (of React) that relies on
    allowSyntheticDefaultImports.
    
    Fixes #1628

--- packages/core/react-dnd/src/common/DndProvider.tsx
+++ packages/core/react-dnd/src/common/DndProvider.tsx
@@ -1,4 +1,5 @@
-import React, { memo } from 'react'
+import * as React from 'react'
+import { memo } from 'react'
 import { BackendFactory, DragDropManager } from 'dnd-core'
 import { DndContext, createDndContext } from './DndContext'
 
