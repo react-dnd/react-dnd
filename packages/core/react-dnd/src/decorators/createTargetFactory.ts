3de4686772cca097c88e90880d2997ce181baa71
Chris Trevino <darthtrevino@users.noreply.github.com>
2019-11-26 18:00:01 -0800

    Replace invariant dependency with @react-dnd/invariant (#1632)
    * build(esm): replace invariant with @react-dnd/invariant
    
    * fix(esm): update react-dnd/invariant version

--- packages/core/react-dnd/src/decorators/createTargetFactory.ts
+++ packages/core/react-dnd/src/decorators/createTargetFactory.ts
@@ -2,7 +2,7 @@ declare const process: any
 
 import * as React from 'react'
 import { DropTarget } from 'dnd-core'
-import invariant from 'invariant'
+import { invariant } from '@react-dnd/invariant'
 import { DropTargetMonitor } from '../interfaces'
 import { isPlainObject } from '../utils/js_utils'
 import { DropTargetSpec } from './interfaces'
--- packages/core/react-dnd/src/hooks/internal/drag.ts
+++ packages/core/react-dnd/src/hooks/internal/drag.ts
@@ -1,5 +1,5 @@
 import { useMemo, MutableRefObject } from 'react'
-import invariant from 'invariant'
+import { invariant } from '@react-dnd/invariant'
 import {
 	DragSourceHookSpec,
 	DragObjectWithType,
