661f6b19f3ff99d1f6eadbad96e02c94ce48061d
darthtrevino <darthtrevino@users.noreply.github.com>
2019-12-07 17:28:16 +0000

    Automated deployment: Sat Dec  7 17:28:16 UTC 2019 8aacb15dbe93a563cf7d96308b6fc0ddea0eda9a

--- examples_decorators_js/01-dustbin/multiple-targets/src/index.js
+++ examples_decorators_js/01-dustbin/multiple-targets/src/index.js
@@ -3,12 +3,12 @@
 	import ReactDOM from 'react-dom'
 	import Example from './example'
 	import { DndProvider } from 'react-dnd'
-	import HTML5Backend from 'react-dnd-html5-backend'
+	import Backend from 'react-dnd-html5-backend'
 
 	function App() {
 		return (
 			<div className="App">
-				<DndProvider backend={HTML5Backend}>
+				<DndProvider backend={Backend}>
 					<Example />
 				</DndProvider>
 			</div>
