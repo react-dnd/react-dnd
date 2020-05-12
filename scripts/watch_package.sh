3bf347302e14d5d0739c21b6abbdd3a708c448d3
Chris Trevino <chtrevin@microsoft.com>
2020-05-12 11:57:46 -0700

    build: disable verbose babel output in package watch mode

--- scripts/watch_package.sh
+++ scripts/watch_package.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 set -e
 npx tsc -w --preserveWatchOutput&
-npx babel --config-file=../../../babel.config.js lib --out-dir dist/esm -w --verbose lib&
-npx babel --config-file=../../../babel.config.cjs.js lib --out-dir dist/cjs -w --verbose lib
+npx babel --config-file=../../../babel.config.js lib --out-dir dist/esm -w&
+npx babel --config-file=../../../babel.config.cjs.js lib --out-dir dist/cjs -w
