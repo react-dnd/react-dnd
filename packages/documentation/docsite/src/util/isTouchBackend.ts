f9d5e058d914bef52b69fa8279406f0f436f4e3f
Chris Trevino <chtrevin@microsoft.com>
2020-05-22 12:35:06 -0700

    docs: add localstorage flag for touchbackend in docs

--- packages/documentation/docsite/src/util/isTouchBackend.ts
+++ packages/documentation/docsite/src/util/isTouchBackend.ts
@@ -3,7 +3,10 @@ import { parse } from 'query-string'
 export function isTouchBackend() {
 	if (typeof window !== 'undefined') {
 		const queryObject = parse(window.location.search)
-		return queryObject.touch !== undefined
+		return (
+			queryObject.touch !== undefined ||
+			localStorage.REACT_DND_TOUCH_BACKEND === 'true'
+		)
 	} else {
 		return false
 	}
