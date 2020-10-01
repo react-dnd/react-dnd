94cfd94a38c021d7c03b14a465297bb236b99000
Junior dos Santos Oliveira Mariano <39390974+futirstanjo@users.noreply.github.com>
2020-10-01 18:36:53 -0300

    for a the best syntax

--- scripts/go_no_go.js
+++ scripts/go_no_go.js
@@ -1,14 +1,17 @@
 /* eslint-disable @typescript-eslint/no-var-requires, no-console */
 const inquirer = require('inquirer')
-inquirer
-	.prompt([
+
+const promptValues = [
 		{
 			type: 'input',
 			default: false,
 			name: 'go',
 			message: 'Is everything ready for publication?',
 		},
-	])
+	]
+
+inquirer
+	.prompt(promptValues)
 	.then(answers => {
 		if (answers.go === 'true') {
 			console.log('🚀')
