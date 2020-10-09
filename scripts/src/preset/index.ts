9298f1fb5c3f9bc17967399a6bc1eb4b9f9fcf4a
Chris Trevino <darthtrevino@gmail.com>
2020-10-09 14:57:26 -0700

    build: move rollup deps to build pkg

--- scripts/src/preset/index.ts
+++ scripts/src/preset/index.ts
@@ -4,13 +4,18 @@ import { defineBuild } from './defineBuild'
 import { defineWatch } from './defineWatch'
 
 export function preset(): Record<string, TaskFunction> {
-	const clean = defineClean()
-	const build = defineBuild()
-	const watch = defineWatch()
+	try {
+		const clean = defineClean()
+		const build = defineBuild()
+		const watch = defineWatch()
 
-	return {
-		clean,
-		build,
-		watch,
+		return {
+			clean,
+			build,
+			watch,
+		}
+	} catch (err) {
+		console.error('error defining tasks', err)
+		throw err
 	}
 }
--- scripts/src/tasks/rollup.ts
+++ scripts/src/tasks/rollup.ts
@@ -1,33 +1,135 @@
 import { noopTask } from './common'
-import { existsSync } from 'fs'
 import { join } from 'path'
-import { task as shellTask } from 'gulp-shell'
-import { TaskFunction } from 'gulp'
+import { parallel, TaskFunction } from 'gulp'
 import { subtaskFail, subtaskSuccess } from '../log'
+import { terser } from 'rollup-plugin-terser'
+import { nodeResolve } from '@rollup/plugin-node-resolve'
+import rollup = require('rollup')
 
-const cwd = process.cwd()
-const rollupConfigPath = join(cwd, 'rollup.config.js')
+/* eslint-disable @typescript-eslint/no-var-requires */
+const commonjs = require('@rollup/plugin-commonjs')
+const replace = require('@rollup/plugin-replace')
+/* eslint-enable @typescript-eslint/no-var-requires */
+
+function getUmdConfiguration(): UmdConfig | undefined {
+	// eslint-disable-next-line
+	const { umd } = require(join(process.cwd(), 'package.json'))
+	return umd
+}
 
 export function rollupBundle(): TaskFunction {
-	return isRollupConfigured() ? execRollup : noopTask
+	const umd = getUmdConfiguration()
+	return umd == null
+		? noopTask
+		: parallel(
+				() =>
+					rollupDebug(umd)
+						.then(() => subtaskSuccess('rollup-dbg'))
+						.catch((err: Error) => {
+							subtaskFail('rollup-dbg')
+							throw err
+						}),
+				() =>
+					rollupMin(umd)
+						.then(() => subtaskSuccess('rollup-min'))
+						.catch((err: Error) => {
+							subtaskFail('rollup-min')
+							throw err
+						}),
+		  )
 }
 
-function execRollup() {
-	return Promise.resolve()
-		.then(() => shellTask(['rollup -c rollup.config.js'], { quiet: true }))
-		.then(() => subtaskSuccess('rollup'))
-		.catch((err) => {
-			subtaskFail(err)
-			throw err
-		})
+interface UmdConfig {
+	name: string
+	external: string[]
+	input: string
+	globals: Record<string, string>
 }
 
-function isRollupConfigured(): boolean {
-	return existsSync(rollupConfigPath)
+async function rollupDebug(config: UmdConfig) {
+	const rcfg = debugConfig(config)
+	const bundle = await rollup.rollup(rcfg)
+	await bundle.write(rcfg)
+}
+async function rollupMin(config: UmdConfig) {
+	const rcfg = minConfig(config)
+	const bundle = await rollup.rollup(rcfg)
+	await bundle.write(rcfg)
 }
 
-export function watchRollup(): TaskFunction {
-	return isRollupConfigured()
-		? () => Promise.resolve()
-		: shellTask(['rollup -c rollup.config.js -w'])
+export function watchRollup() {
+	const umd = getUmdConfiguration()
+	if (umd == null) {
+		return
+	}
+	const handleEvent = (task: string) => {
+		return ({ code }: { code: string }) => {
+			if (code === 'END') {
+				subtaskSuccess(task)
+			}
+			if (code === 'ERROR') {
+				subtaskSuccess(task)
+			}
+		}
+	}
+
+	const mcfg = minConfig(umd)
+	const dcfg = debugConfig(umd)
+	const minWatch = rollup.watch(mcfg)
+	const dbgWatch = rollup.watch(dcfg)
+	minWatch.on('event', handleEvent('rollup-min'))
+	dbgWatch.on('event', handleEvent('rollup-dbg'))
+}
+
+function replaceNodeEnv(env: string) {
+	return replace({
+		values: { 'process.env.NODE_ENV': JSON.stringify(env) },
+		delimiters: ['', ''],
+	})
+}
+
+function debugConfig({
+	name,
+	input,
+	external,
+	globals,
+}: UmdConfig): rollup.RollupOptions {
+	return {
+		input,
+		external,
+		plugins: [
+			nodeResolve({ browser: true }),
+			commonjs(),
+			replaceNodeEnv('development'),
+		],
+		output: {
+			name,
+			file: join(process.cwd(), `dist/umd/${name}.js`),
+			format: 'umd',
+			globals,
+		},
+	}
+}
+function minConfig({
+	name,
+	input,
+	external,
+	globals,
+}: UmdConfig): rollup.RollupOptions {
+	return {
+		input,
+		external,
+		plugins: [
+			nodeResolve({ browser: true }),
+			commonjs(),
+			replaceNodeEnv('production'),
+			terser(),
+		],
+		output: {
+			name,
+			file: join(process.cwd(), `dist/umd/${name}.min.js`),
+			format: 'umd',
+			globals,
+		},
+	}
 }
