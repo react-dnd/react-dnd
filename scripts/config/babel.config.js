ab795850656ff5c86f980bbb427cb57e6e8d8fe1
Chris Trevino <darthtrevino@gmail.com>
2020-10-15 00:33:37 -0700

    build: get unit tests running in pnp

--- scripts/config/babel.config.js
+++ scripts/config/babel.config.js
@@ -1,7 +1,7 @@
 module.exports = {
 	presets: [
 		[
-			require.resolve('@babel/preset-env'),
+			require('@babel/preset-env'),
 			{
 				modules: false,
 				targets: {
@@ -11,8 +11,9 @@ module.exports = {
 		],
 	],
 	plugins: [
-		require.resolve('@babel/plugin-proposal-class-properties'),
-		require.resolve('@babel/plugin-proposal-optional-chaining'),
-		require.resolve('@babel/plugin-proposal-object-rest-spread'),
+		require('@babel/plugin-proposal-class-properties'),
+		require('@babel/plugin-proposal-object-rest-spread'),
+		require('@babel/plugin-proposal-optional-chaining'),
+		require('@babel/plugin-proposal-nullish-coalescing-operator'),
 	],
 }
