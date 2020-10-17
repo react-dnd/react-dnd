767635d4f8036903c2bc5af2f8ea16c8db4f1f56
Chris Trevino <darthtrevino@gmail.com>
2020-10-17 00:03:39 -0700

    build: restore old coverage scope

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,14 @@ module.exports = {
 	setupFilesAfterEnv: ['<rootDir>/jest/setup-enzyme.js'],
 	moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
 	modulePaths: ['<rootDir>/packages/'],
-	collectCoverageFrom: ['packages/*/src/**/*.{ts,tsx}', '!**/__tests__/**'],
+	collectCoverageFrom: [
+		'packages/*/src/**/*.{ts,tsx}',
+		'!**/__tests__/**',
+		'!packages/build/**',
+		'!packages/docsite/**',
+		'!packages/examples-*/**',
+		'!packages/test-*/**',
+	],
 	testMatch: [
 		'<rootDir>/__tests__/**/?(*.)(spec|test).ts(x|)',
 		'<rootDir>/packages/*/src/**/__tests__/**/?(*.)(spec|test).ts(x|)',
