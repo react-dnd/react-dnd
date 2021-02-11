ab6b40ec011cd35f5293b90535f9eb0a813c60af
Chris Trevino <darthtrevino@users.noreply.github.com>
2021-02-10 22:20:42 -0800

    Migrate Chess Example Tests from PR (#3025)
    * test: merge in testing-library based tests for the chessboard example (@ryota-murakami )
    
    * refactor: remove file
    
    * chore: cut semver file

--- packages/examples-decorators/src/00-chessboard/Board.tsx
+++ packages/examples-decorators/src/00-chessboard/Board.tsx
@@ -1,9 +1,10 @@
-import { CSSProperties, FC } from 'react'
+import { CSSProperties, FC, useEffect, useState } from 'react'
 import BoardSquare from './BoardSquare'
+import { Game, Position } from './Game'
 import { Piece } from './Piece'
 
 export interface BoardProps {
-	knightPosition: [number, number]
+	game: Game
 }
 
 /** Styling properties applied to the board element */
@@ -20,16 +21,19 @@ const squareStyle: CSSProperties = { width: '12.5%', height: '12.5%' }
  * The chessboard component
  * @param props The react props
  */
-export const Board: FC<BoardProps> = ({
-	knightPosition: [knightX, knightY],
-}) => {
+export const Board: FC<BoardProps> = ({ game }) => {
+	const [[knightX, knightY], setKnightPos] = useState<Position>(
+		game.knightPosition,
+	)
+	useEffect(() => game.observe(setKnightPos))
+
 	function renderSquare(i: number) {
 		const x = i % 8
 		const y = Math.floor(i / 8)
 
 		return (
 			<div key={i} style={squareStyle}>
-				<BoardSquare x={x} y={y}>
+				<BoardSquare x={x} y={y} game={game}>
 					<Piece isKnight={x === knightX && y === knightY} />
 				</BoardSquare>
 			</div>
