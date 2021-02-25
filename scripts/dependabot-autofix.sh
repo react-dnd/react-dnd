cbe2b445859861812f35b26bfbdfc3cf91a973ae
Chris Trevino <chtrevin@microsoft.com>
2021-02-25 06:17:03 -0800

    ci: work on dependabot-autofix script

--- scripts/dependabot-autofix.sh
+++ scripts/dependabot-autofix.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
-git diff-index --quiet HEAD
-result=$?
-
 set -e
-if [ result == 0 ]
-then
-	echo "no changes detected: $result"  
-else
-	echo "changes detected: $result"
-	git status
+if [[ `git status --porcelain --untracked-files=no` ]]; then
+	echo "changes detected"
 	git add yarn.lock .yarn/cache .pnp.*
 	git commit -m "Dependabot autofix"
 	git push
+else
+	echo "no changes detected"  
 fi
