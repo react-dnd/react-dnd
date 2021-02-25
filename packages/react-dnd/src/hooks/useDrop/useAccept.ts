09a32bb966f334f93bb2b619fa2489106a30d804
Chris Trevino <darthtrevino@users.noreply.github.com>
2021-02-25 09:38:58 -0800

    ci: add github-action to fix dependabot PRs (#3087)
    * ci: add github-action to fix dependabot PRs
    
    * chore: cut semver

--- packages/react-dnd/src/hooks/useDrop/useAccept.ts
+++ packages/react-dnd/src/hooks/useDrop/useAccept.ts
@@ -1,12 +1,14 @@
 import { useMemo } from 'react'
 import { DragObjectWithType, DropTargetHookSpec } from '../types'
 
+/**
+ * Internal utility hook to get an array-version of spec.accept.
+ * The main utility here is that we aren't creating a new array on every render if a non-array spec.accept is passed in.
+ * @param spec
+ */
 export function useAccept<O extends DragObjectWithType, R, P>(
 	spec: DropTargetHookSpec<O, R, P>,
 ) {
-	const specAccept = spec.accept
-	return useMemo(
-		() => (Array.isArray(specAccept) ? specAccept : [specAccept]),
-		[specAccept],
-	)
+	const { accept } = spec
+	return useMemo(() => (Array.isArray(accept) ? accept : [accept]), [accept])
 }
