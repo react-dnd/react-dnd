73e0e0a9f28d41e72ad9534448e88da4a0612880
Chris Trevino <chtrevin@microsoft.com>
2022-02-04 18:38:51 -0800

    test: eliminate duplication in the modules tests

--- module_test/cjs-imports.cjs
+++ module_test/cjs-imports.cjs
@@ -6,55 +6,27 @@ const touchBackend = require('react-dnd-touch-backend')
 const testBackend = require('react-dnd-test-backend')
 const testUtils = require('react-dnd-test-utils')
 
-if (core.createDragDropManager == null || core.HandlerRole == null) {
-	throw new Error('missing exports in core')
+const api = require('./api.json')
+
+function check(imported, libKey) {
+	console.log('checking', libKey)
+	Object.keys(imported).forEach(key => {
+		if (!imported[key]) {
+			throw new Error(`missing export: ${key}`)
+		}
+	})
+	Object.keys(imported).forEach(key => {
+		if (!api[libKey][key]) {
+			throw new Error(`uneexpected export: ${key}`)
+		}
+	})
 }
 
-if (
-	dnd.DndContext == null ||
-	dnd.DndProvider == null ||
-	dnd.DragPreviewImage == null ||
-	dnd.useDrag == null ||
-	dnd.useDragDropManager == null ||
-	dnd.useDragLayer == null ||
-	dnd.useDrop == null
-) {
-	throw new Error('missing exports in dnd')
-}
-
-if (
-	htmlBackend.HTML5Backend == null ||
-	htmlBackend.NativeTypes == null ||
-	htmlBackend.getEmptyImage == null
-) {
-	throw new Error('missing exports in html-abckend')
-}
-
-if (
-	touchBackend.ListenerType == null ||
-	touchBackend.TouchBackend == null ||
-	touchBackend.TouchBackendImpl == null
-) {
-	throw new Error('missing exports in touch-backend')
-}
-
-if (testBackend.TestBackend == null || testBackend.TestBackendImpl == null) {
-	throw new Error('missing exports in test-backend')
-}
-
-if (
-	testUtils.fireDrag == null ||
-	testUtils.fireDragDrop == null ||
-	testUtils.fireDragHover == null ||
-	testUtils.getHandlerId == null ||
-	testUtils.simulateDrag == null ||
-	testUtils.simulateDragDrop == null ||
-	testUtils.simulateDragHover == null ||
-	testUtils.tick == null ||
-	testUtils.wrapWithBackend == null ||
-	testUtils.wrapWithTestBackend == null
-) {
-	throw new Error('missing exports in test-utils')
-}
+check(core, 'core')
+check(dnd, 'dnd')
+check(htmlBackend, 'htmlBackend')
+check(touchBackend, 'touchBackend')
+check(testBackend, 'testBackend')
+check(testUtils, 'testUtils')
 
 console.log('👍 CommonJS Modules OK')
--- module_test/mjs-imports.mjs
+++ module_test/mjs-imports.mjs
@@ -5,56 +5,33 @@ import * as htmlBackend from 'react-dnd-html5-backend'
 import * as touchBackend from 'react-dnd-touch-backend'
 import * as testBackend from 'react-dnd-test-backend'
 import * as testUtils from 'react-dnd-test-utils'
+import { readFile } from 'fs/promises';
 
-if (core.createDragDropManager == null || core.HandlerRole == null) {
-	throw new Error('missing exports in core')
+const api = JSON.parse(
+  await readFile(
+    new URL('./api.json', import.meta.url)
+  )
+);
+
+function check(imported, libKey) {
+	console.log('checking', libKey)
+	Object.keys(imported).forEach(key => {
+		if (!imported[key]) {
+			throw new Error(`missing export: ${key}`)
+		}
+	})
+	Object.keys(imported).forEach(key => {
+		if (!api[libKey][key]) {
+			throw new Error(`uneexpected export: ${key}`)
+		}
+	})
 }
 
-if (
-	dnd.DndContext == null ||
-	dnd.DndProvider == null ||
-	dnd.DragPreviewImage == null ||
-	dnd.useDrag == null ||
-	dnd.useDragDropManager == null ||
-	dnd.useDragLayer == null ||
-	dnd.useDrop == null
-) {
-	throw new Error('missing exports in dnd')
-}
-
-if (
-	htmlBackend.HTML5Backend == null ||
-	htmlBackend.NativeTypes == null ||
-	htmlBackend.getEmptyImage == null
-) {
-	throw new Error('missing exports in html-abckend')
-}
-
-if (
-	touchBackend.ListenerType == null ||
-	touchBackend.TouchBackend == null ||
-	touchBackend.TouchBackendImpl == null
-) {
-	throw new Error('missing exports in touch-backend')
-}
-
-if (testBackend.TestBackend == null || testBackend.TestBackendImpl == null) {
-	throw new Error('missing exports in test-backend')
-}
-
-if (
-	testUtils.fireDrag == null ||
-	testUtils.fireDragDrop == null ||
-	testUtils.fireDragHover == null ||
-	testUtils.getHandlerId == null ||
-	testUtils.simulateDrag == null ||
-	testUtils.simulateDragDrop == null ||
-	testUtils.simulateDragHover == null ||
-	testUtils.tick == null ||
-	testUtils.wrapWithBackend == null ||
-	testUtils.wrapWithTestBackend == null
-) {
-	throw new Error('missing exports in test-utils')
-}
+check(core, 'core')
+check(dnd, 'dnd')
+check(htmlBackend, 'htmlBackend')
+check(touchBackend, 'touchBackend')
+check(testBackend, 'testBackend')
+check(testUtils, 'testUtils')
 
 console.log('👍 ESModules OK')
