19b26a21a89d93fff5f71a1bcbd551a30c3fb980
Chris Trevino <chtrevin@microsoft.com>
2022-02-04 18:42:52 -0800

    chore: formatting

--- module_test/cjs-imports.cjs
+++ module_test/cjs-imports.cjs
@@ -10,12 +10,12 @@ const api = require('./api.json')
 
 function check(imported, libKey) {
 	console.log('checking', libKey)
-	Object.keys(imported).forEach(key => {
+	Object.keys(imported).forEach((key) => {
 		if (!imported[key]) {
 			throw new Error(`missing export: ${key}`)
 		}
 	})
-	Object.keys(imported).forEach(key => {
+	Object.keys(imported).forEach((key) => {
 		if (!api[libKey][key]) {
 			throw new Error(`uneexpected export: ${key}`)
 		}
--- module_test/mjs-imports.mjs
+++ module_test/mjs-imports.mjs
@@ -5,22 +5,18 @@ import * as htmlBackend from 'react-dnd-html5-backend'
 import * as touchBackend from 'react-dnd-touch-backend'
 import * as testBackend from 'react-dnd-test-backend'
 import * as testUtils from 'react-dnd-test-utils'
-import { readFile } from 'fs/promises';
+import { readFile } from 'fs/promises'
 
-const api = JSON.parse(
-  await readFile(
-    new URL('./api.json', import.meta.url)
-  )
-);
+const api = JSON.parse(await readFile(new URL('./api.json', import.meta.url)))
 
 function check(imported, libKey) {
 	console.log('checking', libKey)
-	Object.keys(imported).forEach(key => {
+	Object.keys(imported).forEach((key) => {
 		if (!imported[key]) {
 			throw new Error(`missing export: ${key}`)
 		}
 	})
-	Object.keys(imported).forEach(key => {
+	Object.keys(imported).forEach((key) => {
 		if (!api[libKey][key]) {
 			throw new Error(`uneexpected export: ${key}`)
 		}
