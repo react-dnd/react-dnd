18701eea1a3204903927f257180589378a96b0c9
Chris Trevino <darthtrevino@gmail.com>
2023-01-19 10:22:07 -0800

    run formatter

--- scripts/createDistPkgJson.mjs
+++ scripts/createDistPkgJson.mjs
@@ -7,27 +7,36 @@ if (!process.cwd().match(/packages\/[^/]+$/)) {
 
 const DIST_FOLDERS = [
 	{ folder: 'esm', type: 'module' },
-	{ folder: 'cjs', type: 'commonjs' }
+	{ folder: 'cjs', type: 'commonjs' },
 ]
 const PACKAGE_JSON_PATH = path.join(process.cwd(), 'package.json')
 const JSON_ENCODING = { encoding: 'utf-8' }
 
-const toJson = obj => JSON.stringify(obj, null, 2)
-const readJson = async file => JSON.parse(await fs.readFile(file, JSON_ENCODING))
-const writeJson = async (file, content) => fs.writeFile(file, content, JSON_ENCODING)
+const toJson = (obj) => JSON.stringify(obj, null, 2)
+const readJson = async (file) =>
+	JSON.parse(await fs.readFile(file, JSON_ENCODING))
+const writeJson = async (file, content) =>
+	fs.writeFile(file, content, JSON_ENCODING)
 
 /**
- * This function injects a package.json file into dist/esm and dist/cjs to 
+ * This function injects a package.json file into dist/esm and dist/cjs to
  * force node into a specific module resolution mode.
  */
 async function injectResolverPackageJsonFiles() {
 	const pkgJson = await readJson(PACKAGE_JSON_PATH)
 	const name = pkgJson.name
 
-	return Promise.all(DIST_FOLDERS.map(({ folder, type }) => {
-		const distPkgJsonPath = path.join(process.cwd(), 'dist', folder, 'package.json')
-		return writeJson(distPkgJsonPath, toJson({ name, type }))
-	}))
+	return Promise.all(
+		DIST_FOLDERS.map(({ folder, type }) => {
+			const distPkgJsonPath = path.join(
+				process.cwd(),
+				'dist',
+				folder,
+				'package.json',
+			)
+			return writeJson(distPkgJsonPath, toJson({ name, type }))
+		}),
+	)
 }
 
 injectResolverPackageJsonFiles()
